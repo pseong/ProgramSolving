673faa37cd83d11c129690c94ae02cc63ee0e82a
Guest <guest-p9xl7b@h29038.sgb.skir.kr>
2021-01-28 11:02:31 +0900

    2475

---
+++ boj/2475.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0); 
+    cin.tie(0); cout.tie(0);
+
+    int a, b, c, d, e;
+    cin >> a >> b >> c >> d >> e;
+    cout << (a*a + b*b + c*c + d*d + e*e)%10;
+}
