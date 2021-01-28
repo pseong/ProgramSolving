c177ec87c408719fd97dce6860f82b28629b3870
Guest <guest-fzlbmk@h29036.sgb.skir.kr>
2021-01-28 10:08:57 +0900

    1157

--- boj/1157.cpp
+++ boj/1157.cpp
@@ -8,24 +8,22 @@ int main() {
     cout.tie(0);
 
     int many[200]{ 0 };
-
     bool what = false;
     int ans = 0;
 
-    char c[1000000]{ 0 };
-    for(int i = 0; i < 1000000; i++) {
-        c[i] = getchar();
-    }
-    for(int i = 0; i < 1000000; i++) {
-        if(c[i] == 0) continue;
-        many[c[i]]++;
-        if(many[ans] == many[c[i]] && ans != c[i])
+    string s;
+    cin >> s;
+    for(int i = 0; i < s.size(); i++) {
+        char c = toupper(s[i]);
+        many[c]++;
+        if(many[ans] == many[c] && ans != c)
             what = true;
-        if(many[ans] < many[c[i]]) {
-            ans = c[i];
+        else if(many[ans] < many[c]) {
+            ans = c;
             what = false;
         }
+        if(ans == c) what = false;
     }
     if(what) cout << '?';
-    else cout << (char)toupper(ans);
+    else cout << (char)ans;
 }
