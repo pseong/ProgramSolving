f18edf56bd504e7cf9acd9d3738ffaf3786e6d06
Guest <guest-f7hqz5@h29038.sgb.skir.kr>
2021-01-28 10:29:20 +0900

    1330

--- boj/1330.cpp
+++ boj/1330.cpp
@@ -5,4 +5,10 @@ using namespace std;
 int main() {
     ios::sync_with_stdio(0); 
     cin.tie(0); cout.tie(0);
+
+    int a, b;
+    cin >> a >> b;
+    if(a > b) cout << '>';
+    else if(a < b) cout << '<';
+    else cout << "==";
 }
