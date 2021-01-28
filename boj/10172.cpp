44f0fb5fe5318286c9a5065eed013aa964c5203d
Guest <guest-p9xl7b@h29038.sgb.skir.kr>
2021-01-28 12:22:31 +0900

    10172

---
+++ boj/10172.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0); 
+    cin.tie(0); cout.tie(0);
+
+    cout << "|\\_/|" << endl;
+    cout << "|q p|   /}" << endl;
+    cout << "( 0 )\"\"\"\\" << endl;
+    cout << "|\"^\"`    |" << endl;
+    cout << "||_/=\\\\__|" << endl;
+}
