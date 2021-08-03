7e8e5474cfb921098cf21f99c24908b0fa912b2f
pseong <pkseonguk@gmail.com>
2021-08-03 21:19:24 +0900

    Modify 14002

--- boj/14002.cpp
+++ boj/14002.cpp
@@ -2,6 +2,17 @@
 
 using namespace std;
 
+int dp[1001][2]{ 0 };
+int a[1001]{ 0 };
+
+void go(int p) {
+    if (p == 0) {
+        return;
+    }
+    go(dp[p][1]);
+    cout << a[p] << ' ';
+}
+
 int main() {
     ios::sync_with_stdio(0); 
     cin.tie(0); 
@@ -9,16 +20,15 @@ int main() {
 
     int n;
     cin >> n;
-    int a[1001]{ 0 };
     for(int i = 1; i <= n; i++) {
         cin >> a[i];
     }
 
     int result = 1;
-    int dp[1001][2]{ 0 };
+    
     for(int i = 1; i <= n; i++) {
         dp[i][0] = 1;
-        dp[i][1] = i;
+        dp[i][1] = 0;
         for(int j = 1; j < i; j++) {
             if(a[i] > a[j] && dp[i][0] < dp[j][0] + 1) {
                 dp[i][0] = dp[j][0] + 1;
@@ -30,19 +40,6 @@ int main() {
         }
     }
 
-    int ans[1000]{ 0 };
-    int cnt = 0;
-    int ans2 = result;
-    while(dp[result][1] != result) {
-        ans[cnt] = a[result];
-        result = dp[result][1];
-        cnt++;
-    }
-    ans[cnt] = a[result];
-    cnt++;
-
-    cout << dp[ans2][0] << '\n';
-    for(int i = cnt - 1; i >= 0; i--) {
-        cout << ans[i] << ' ';
-    }
+    cout << dp[result][0] << '\n';
+    go(result);
 }
