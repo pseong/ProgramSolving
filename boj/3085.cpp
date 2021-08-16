86daa8a7d71d048e08700e76f09c49bb5c2a184d
pseong <pkseonguk@gmail.com>
2021-08-16 20:52:02 +0900

    3085

--- boj/3085.cpp
+++ boj/3085.cpp
@@ -2,14 +2,32 @@
 
 using namespace std;
 
+char a[51][51]{0};
+int n = 0;
+int ans = 0;
+void scan() {
+    for(int i=1; i<=n; i++) {
+        int res = 1;
+        for(int j=1; j<=n; j++) {
+            if(a[i][j] == a[i][j-1]) res++;
+            else res = 1;
+            if(res > ans) ans = res;
+        }
+        res = 1;
+        for(int j=1; j<=n; j++) {
+            if(a[j][i] == a[j-1][i]) res++;
+            else res = 1;
+            if(res > ans) ans = res;
+        }
+    }
+}
+
 int main() {
     ios::sync_with_stdio(0); 
     cin.tie(0); cout.tie(0);
 
-    int n;
     cin >> n;
 
-    char a[51][51]{0};
     for(int i=1; i<=n; i++) {
         for(int j=1; j<=n; j++) {
             cin >> a[i][j];
@@ -18,7 +36,18 @@ int main() {
 
     for(int i=1; i<=n; i++) {
         for(int j=1; j<=n; j++) {
-            cin >> a[i][j];
+            if(i != n) {
+                swap(a[i][j], a[i+1][j]);
+                scan();
+                swap(a[i][j], a[i+1][j]);
+            }
+            if(j != n) {
+                swap(a[i][j], a[i][j+1]);
+                scan();
+                swap(a[i][j], a[i][j+1]);
+            }
         }
     }
+
+    cout << ans;
 }
