243b8f7c2f6dab24d8c35f25ad01bf90fd433ad6
pseong <pkseonguk@gmail.com>
2021-08-26 20:54:22 +0900

    ADd 15665

---
+++ boj/15665.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int n, m;
+vector<int> pre;
+vector<int> v;
+int a[9]{0};
+
+void show() {
+    if(v == pre) return;
+    pre = v;
+    for(int i : v) {
+        cout << i << ' ';
+    }
+    cout << '\n';
+}
+
+void func(int M) {
+    if(M < 1) {
+        show();
+        return;
+    }
+    int pre = 0;
+    for(int i=1; i<=n; i++) {
+        if(a[pre] == a[i]) continue;
+        v.push_back(a[i]);
+        func(M-1);
+        v.pop_back();
+        pre = i;
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0); 
+    cin.tie(0); 
+    cout.tie(0);
+
+    cin >> n >> m;
+    for(int i=1; i<=n; i++) {
+        cin >> a[i];
+    }
+    sort(a+1, a+n+1);
+    func(m);
+}
