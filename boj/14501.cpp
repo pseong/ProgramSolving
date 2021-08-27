84e200d5bad8496ed93c944dc918c32ad67c3630
pseong <pkseonguk@gmail.com>
2021-08-27 14:17:40 +0900

    Add 14501

---
+++ boj/14501.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int n;
+int t[25]{0};
+int p[25]{0};
+int ans=0;
+
+void func(int now, int money) {
+    if(now == n+1) {
+        if(ans<money) ans = money;
+        return;
+    }
+    if(now+t[now] > n+1) {
+        if(ans<money) ans = money;
+        if(now < n+1) {
+            func(now+1, money);
+        }
+        return;
+    }
+    func(now+t[now], money+p[now]);
+    func(now+1, money);
+}
+
+int main() {
+    ios::sync_with_stdio(0); 
+    cin.tie(0); 
+    cout.tie(0);
+
+    cin >> n;
+
+    for(int i=1; i<=24; i++) {
+        t[i] = 100;
+    }
+
+    for(int i=1; i<=n; i++) {
+        cin >> t[i];
+        cin >> p[i];
+    }
+    
+    func(1, 0);
+    cout << ans;
+}
