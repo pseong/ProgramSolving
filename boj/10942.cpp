b5a35950e632f6add51bf479c9483965e8130ccd
pseong <pkseonguk@gmail.com>
2021-09-05 02:10:04 +0900

    Add 10942

---
+++ boj/10942.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+bool pal[3000][3000]{0};
+int an[3000]{0};
+
+int main() {
+    ios::sync_with_stdio(0); 
+    cin.tie(0); 
+    cout.tie(0);
+
+    int n;
+    cin >> n;
+    for(int i=1; i<=n; i++) {
+        cin >> an[i];
+    }
+
+    for(int i=1; i<=n; i++) {
+        int go=i;
+        int back=i;
+        while(go<=n && back>=1 && an[go]==an[back]) {
+            pal[back][go] = 1;
+            go++;
+            back--;
+        }
+    }
+
+    int m;
+    cin >> m;
+    for(int i=0; i<m; i++) {
+        int a, b;
+        cin >> a >> b;
+        cout << pal[a][b] << '\n';
+    }
+}
