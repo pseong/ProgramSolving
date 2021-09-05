e38a249d1338672b9edf5ed471bca06fa2a740cc
pseong <pkseonguk@gmail.com>
2021-09-05 23:53:52 +0900

    Add 15989

---
+++ boj/15989.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <queue>
+#include <tuple>
+#include <algorithm>
+#include <cstring>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int t;
+    cin >> t;
+
+    int dp[10001]{0};
+    dp[0] = 1;
+    for(int i=1; i<=10000; i++) {
+        dp[i] += dp[i-1];
+    }
+    for(int i=2; i<=10000; i++) {
+        dp[i] += dp[i-2];
+    }
+    for(int i=3; i<=10000; i++) {
+        dp[i] += dp[i-3];
+    }
+    while(t--) {
+        int n;
+        cin >> n;
+        cout << dp[n] << '\n';
+    }
+}
