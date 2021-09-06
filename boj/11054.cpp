85081d6a5f0d0f5c0768cbf9fd897de1d1c9b5d2
pseong <pkseonguk@gmail.com>
2021-09-06 23:05:15 +0900

    Modify 11054

--- boj/11054.cpp
+++ boj/11054.cpp
@@ -1,11 +1,10 @@
-#include <bits/stdc++.h>
+#include <iostream>
 
 using namespace std;
 
 int main() {
     ios::sync_with_stdio(0); 
-    cin.tie(0); 
-    cout.tie(0);
+    cin.tie(0); cout.tie(0);
 
     int n;
     cin >> n;
