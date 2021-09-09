a9cd41d44169f9bd862adad15ababe89e14780f4
pseong <pkseonguk@gmail.com>
2021-09-09 17:28:27 +0900

    Add 2263

---
+++ boj/2263.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+int n;
+int inorder[100000]{0};
+int postorder[100000]{0};
+int index[100000]{0};
+
+using namespace std;
+
+void preOrder(int inS, int inE, int postS, int postE) {
+    if(inS>inE||postS>postE) return;
+    int root = postorder[postE];
+    cout << root << ' ';
+    preOrder(inS, index[root]-1, postS, postS+index[root]-inS-1);
+    preOrder(index[root]+1, inE, postS+index[root]-inS, postE-1);
+}
+
+int main() {
+    ios::sync_with_stdio(0); 
+    cin.tie(0); cout.tie(0);
+
+    cin >> n;
+    for(int i=0; i<n; i++) {
+        cin >> inorder[i];
+    }
+    for(int i=0; i<n; i++) {
+        cin >> postorder[i];
+    }
+    for(int i=0; i<n; i++) {
+        index[inorder[i]] = i;
+    }
+    preOrder(0, n-1, 0, n-1);
+}
