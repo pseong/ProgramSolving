bcb980760ef83856b67ffe848c2c181d2e4b2a47
pseong <pkseonguk@gmail.com>
2021-09-09 17:03:43 +0900

    Modify 1904

--- boj/1904.cpp
+++ boj/1904.cpp
@@ -13,7 +13,7 @@ int main() {
     dp[0]=1;
     dp[1]=1;
     for(int i=2; i<=n; i++) {
-        dp[i] = dp[i-1] + dp[i-2];
+        dp[i] = (dp[i-1] + dp[i-2])%15746;
     }
     cout << dp[n];
 }
