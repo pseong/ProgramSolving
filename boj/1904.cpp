501be285a982c589e2bdc4bcb95a946e7aef67b8
pseong <pkseonguk@gmail.com>
2021-09-09 16:41:09 +0900

    Add 1904

---
+++ boj/1904.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int dp[1000001]{0};
+int main() {
+    ios::sync_with_stdio(0); 
+    cin.tie(0); cout.tie(0);
+
+    int n;
+    cin >> n;
+    
+    dp[0]=1;
+    dp[1]=1;
+    for(int i=2; i<=n; i++) {
+        dp[i] = dp[i-1] + dp[i-2];
+    }
+    cout << dp[n];
+}
