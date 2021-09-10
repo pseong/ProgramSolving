783cf85b717bfe364f328470be092b0bcd083a2f
pseong <pkseonguk@gmail.com>
2021-09-10 12:16:33 +0900

    Add 4673

---
+++ boj/4673.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int d(int n) {
+    int sum=n;
+    while(n!=0) {
+        sum = sum+n%10;
+        n /= 10;
+    }
+    return sum;
+}
+
+bool ans[10050]{0};
+int main() {
+    ios::sync_with_stdio(0); 
+    cin.tie(0); cout.tie(0);
+    
+    for(int i=100; i<=10000; i++) {
+        ans[d(i)] = true;
+    }
+    for(int i=1; i<=10000; i++) {
+        if(!ans[i]) cout << i << '\n';
+    }
+}
