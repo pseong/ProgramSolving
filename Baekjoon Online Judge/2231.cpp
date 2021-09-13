553c66c30877c0984c8cf622f9fda9d10120d9fb
pseong <pkseonguk@gmail.com>
2021-09-13 16:42:20 +0900

    Add 2231

---
+++ Baekjoon Online Judge/2231.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int d(int n) {
+    int sum=n;
+    while(n!=0) {
+        sum = sum+n%10;
+        n /= 10;
+    }
+    return sum;
+}
+
+int ans[1500000]{0};
+int main() {
+    ios::sync_with_stdio(0); 
+    cin.tie(0); cout.tie(0);
+    int n;
+    cin >> n;
+    for(int i=1;i<=696933;i++) {
+        if(i==696933) {
+            cout << "goood::::::" << d(i) << '\n';
+        }
+        if(!ans[d(i)]) ans[d(i)] = i;
+        else {
+            if(ans[d(i)] > i) ans[d(i)] = i;
+        }
+    }
+    cout << ans[n];
+}
