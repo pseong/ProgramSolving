ebede955047e585e406e6481709e583a191eaa87
pseong <pkseonguk@gmail.com>
2021-09-14 12:38:05 +0900

    Modify 1000

--- Baekjoon Online Judge/1000.cpp
+++ Baekjoon Online Judge/1000.cpp
@@ -1,8 +1,12 @@
-#include <bits/stdc++.h>
+#include <iostream>
 
 using namespace std;
 
 int main() {
     ios::sync_with_stdio(0); 
     cin.tie(0); cout.tie(0);
+
+    int a, b;
+    cin >> a >> b;
+    cout << a+b;
 }
