b91d476984feb11c778a7f16f70fa8381e7b8a1d
pseong <pkseonguk@gmail.com>
2021-09-15 23:49:03 +0900

    Add 5086

---
+++ Baekjoon Online Judge/5086.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0); 
+    cin.tie(0); cout.tie(0);
+    while(true) {
+        int a, b;
+        cin >> a >> b;
+        if(a==0&&b==0) break;
+        if(b%a==0) cout << "factor\n";
+        else if(a%b==0) cout << "multiple\n";
+        else cout << "neither";
+    }
+}
