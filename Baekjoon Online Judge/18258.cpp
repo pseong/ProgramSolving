4e763a18f89d550014ce38d4357525a97c649040
pseong <pkseonguk@gmail.com>
2021-09-16 21:00:43 +0900

    Add 18258

---
+++ Baekjoon Online Judge/18258.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0); 
+    cin.tie(0); cout.tie(0);
+
+    queue<int> q;
+    int n;
+    cin >> n;
+    while(n--) {
+        string s;
+        cin >> s;
+        if(s=="push") {
+            int x;
+            cin >> x;
+            q.push(x);
+        } else if(s=="pop") {
+            if(q.empty()) cout << -1 << '\n';
+            else {
+                cout << q.front() << '\n';
+                q.pop();
+            }
+        } else if(s=="size") {
+            cout << q.size() << '\n';
+        } else if(s=="empty") {
+            cout << q.empty() << '\n';
+        } else if(s=="front") {
+            if(q.empty()) cout << -1 << '\n';
+            else cout << q.front() << '\n';
+        } else if(s=="back") {
+            if(q.empty()) cout << -1 << '\n';
+            else cout << q.back() << '\n';
+        }
+    }
+}
