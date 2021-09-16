e3aa4860bca8d981586ad8eae549f511832ce26f
pseong <pkseonguk@gmail.com>
2021-09-16 18:58:13 +0900

    Add 2981

---
+++ Baekjoon Online Judge/2981.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int gcd(int a, int b) {
+    while(b!=0) {
+        a = a%b;
+        swap(a, b);
+    }
+    return a;
+}
+
+int main() {
+    ios::sync_with_stdio(0); 
+    cin.tie(0); cout.tie(0);
+
+    int an[150]{0};
+    int n;
+    cin >> n;
+    int g=-1; //최대 공약수
+    for(int i=0; i<n; i++) {
+        cin >> an[i];
+    }
+    vector<int> diff;
+    for(int i=0; i<n; i++) {
+        for(int j=i+1; j<n; j++) {
+            diff.push_back(abs(an[i]-an[j]));
+        }
+    }
+    sort(diff.begin(), diff.end());
+    int a=diff[0];
+    for(int i=1; i<diff.size(); i++) {
+        a=gcd(a, diff[i]);
+    }
+    vector<int> v;
+    for(int i=1; i*i<=a; i++) {
+        if(i*i==a) {
+            v.push_back(i);
+        }
+        else if(a%i==0) {
+            v.push_back(i);
+            v.push_back(a/i);
+        }
+    }
+    sort(v.begin(), v.end());
+    for(int i=1; i<v.size(); i++) {
+        cout << v[i] << ' ';
+    }
+}
