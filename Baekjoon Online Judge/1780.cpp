451597129752867e2064a5629edd38ce5899fec4
pseong <pkseonguk@gmail.com>
2021-09-17 18:53:32 +0900

    Add 1780

--- Baekjoon Online Judge/1780.cpp
+++ Baekjoon Online Judge/1780.cpp
@@ -1,44 +1,44 @@
-#include <bits/stdc++.h>
+#include <iostream>
 
 using namespace std;
 
-int mp[2200][2200]{ 0 };
-int result[3]{ 0 };
+int an[2500][2500]{0};
+int ans[3]{0};
+int n;
 
-bool valid(int i, int j, int i2, int j2) {
-    int pre = mp[i][j];
-    for(int n = i; n <= i2; n++) {
-        for(int m = j; m <= j2; m++) {
-            if(pre != mp[n][m]) return false;
+void f(int x1, int y1, int x2,  int y2) {
+    int a=-2;
+    bool one=true;
+    for(int i=x1; i<=x2; i++) {
+        for(int j=y1; j<=y2; j++) {
+            if(a==-2) a = an[i][j];
+            else if(a != an[i][j]) one=false;
         }
     }
-    result[++pre]++;
-    return true;
-}
-
-void partition(int i, int j, int i2, int j2) {
-    if(valid(i, j, i2, j2)) return;
-    int third = (i2 - i + 1) / 3;
-    for(int n = i; n <= i2; n += third) {
-        for(int m = j; m <= j2; m += third) {
-            partition(n, m, n + third - 1, m + third - 1);
+    if(one) ans[a]++;
+    else {
+        int gap=(x2-x1+1)/3;
+        for(int i=x1; i<=x2-gap+1; i+=gap) {
+            for(int j=y1; j<=y2-gap+1; j+=gap) {
+                f(i,j,i+gap-1,j+gap-1);
+            }
         }
     }
 }
 
 int main() {
     ios::sync_with_stdio(0);
-    cin.tie(0);
+    cin.tie(0); cout.tie(0);
 
-    int n;
     cin >> n;
-    for(int i = 1; i <= n; i++) {
-        for(int j = 1; j <= n; j++) {
-            cin >> mp[i][j];
+    for(int i=1; i<=n; i++) {
+        for(int j=1; j<=n; j++) {
+            cin >> an[i][j];
+            an[i][j]++;
         }
     }
-    partition(1, 1, n, n);
-    for(int i = 0; i < 3; i++) {
-        cout << result[i] << '\n';
+    f(1,1,n,n);
+    for(int i=0; i<3; i++) {
+        cout << ans[i] << '\n';
     }
 }
