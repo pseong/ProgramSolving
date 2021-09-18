84353d413b5caf4cc4bb6adb238e641170c239f5
pseong <pkseonguk@gmail.com>
2021-09-19 02:36:03 +0900

    Add 1629

---
+++ Baekjoon Online Judge/1629.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+long long a, b, c;
+
+long long f(long long x, long long y) {
+    if(y==0) return 1;
+    if(y==1) return x;
+    if(x%2==1) return f(x, y-1)*x;
+    long long mid = f(x, y/2)%c;
+    return (mid*mid)%c;
+}
+
+int main() {
+    ios::sync_with_stdio(0); 
+    cin.tie(0); cout.tie(0);
+
+    cin >> a >> b >> c;
+    cout << f(a, b)%c;
+}
