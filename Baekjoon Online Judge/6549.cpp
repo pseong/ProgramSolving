a23fa8e416c16a97bf709797f1eaffd75f1f51a1
pseong <pkseonguk@gmail.com>
2021-09-21 15:10:29 +0900

    Modify 6549

--- Baekjoon Online Judge/6549.cpp
+++ Baekjoon Online Judge/6549.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 #include <algorithm>
-#include <cstring>
 
 using namespace std;
 
 int an[100010]{0};
-int seg[300010]{0};
+int seg[300000]{0};
 int n;
 
 int init(int node, int s, int e) {
@@ -17,7 +16,7 @@ int init(int node, int s, int e) {
     return seg[node] = a;
 }
 
-int query(int node, int s, int e, int l, int r) {
+int query(int node, int  s, int e, int l, int r) {
     if (e<l || r<s) return 0;
     if (l<=s && e<=r) return seg[node];
     int mid = (s + e) / 2;
@@ -27,11 +26,11 @@ int query(int node, int s, int e, int l, int r) {
     return a; 
 }
 
-int go(int l, int r) {
+long long go(int l, int r) {
     if(r-l<0) return 0;
     if(r-l==0) return an[l];
     int mid=query(1, 1, n, l, r);
-    int res=an[mid]*(r-l+1);
+    long long res=(long long)an[mid]*(r-l+1);
     if(l<=mid-1) res=max(res, go(l, mid-1));
     if(r>=mid+1) res=max(res, go(mid+1, r));
     return res;
@@ -41,15 +40,13 @@ int main() {
     ios::sync_with_stdio(0); 
     cin.tie(0); cout.tie(0);
 
+    an[0] = 2000000000;
     while(true) {
         cin >> n;
         if(n==0) break;
-        memset(an, 0, sizeof(an));
         for(int i=1; i<=n; i++) {
             cin >> an[i];
         }
-        an[0] = 2000000000;
-        memset(seg, 0, sizeof(seg));
         init(1,1,n);
         cout << go(1, n) << '\n';
     }
