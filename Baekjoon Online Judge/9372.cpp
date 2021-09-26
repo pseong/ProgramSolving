684dc642ff8363f5aae67b41fbea8865361092a9
pseong <pkseonguk@gmail.com>
2021-09-27 01:19:06 +0900

    Add 9372

---
+++ Baekjoon Online Judge/9372.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int t;
+    cin >> t;
+    while(t--) {
+        int n, m;
+        cin >> n >> m;
+        while(m--) {
+            int a, b;
+            cin >> a >> b;
+        }
+        cout << n-1 << '\n';
+    }
+}
