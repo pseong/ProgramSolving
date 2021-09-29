c62d54a30ae2aaaca6d52dfd67c9257beee1191b
pseong <pkseonguk@gmail.com>
2021-09-30 00:32:03 +0900

    Add Default Code 0000.cpp

---
+++ Baekjoon Online Judge/0000.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <cstring>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <tuple>
+
+typedef long long ll;
+typedef long double ld;
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0); 
+    cin.tie(0); cout.tie(0);
+}
