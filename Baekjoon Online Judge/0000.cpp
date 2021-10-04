110e006a4256884c7c5f78a420c2e751e17d4a90
pseong <pkseonguk@gmail.com>
2021-10-04 14:07:11 +0900

    Modify 0000.cpp

--- Baekjoon Online Judge/0000.cpp
+++ Baekjoon Online Judge/0000.cpp
@@ -17,7 +17,7 @@ using ld = long double;
 using pii = pair<int, int>;
 using pll = pair<long long, long long>;
 using tiii = tuple<int, int, int>;
-using tlll = tuple<long long, long long, long long>
+using tlll = tuple<long long, long long, long long>;
 
 int main() {
     ios::sync_with_stdio(0); 
