911f65daafd4e95f38d193eb8ed6ddc0bb6dc81d
pseong <pkseonguk@gmail.com>
2021-10-07 01:25:28 +0900

    Add 12844

---
+++ Baekjoon Online Judge/12844.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <string>
+#include <cstring>
+#include <cmath>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <tuple>
+#include <list>
+#include <map>
+#include <set>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<long long, long long>;
+using tiii = tuple<int, int, int>;
+using tlll = tuple<long long, long long, long long>;
+
+const int N = 1000;
+int an[N+1]{0};
+ll st[4*N]{0};
+ll lz[4*N]{0};
+
+ll init(int i, int l, int r) {
+    if (l==r) return st[i] = an[l];
+    int m=l+r>>1;
+    return st[i] = init(i*2, l, m)^init(i*2+1, m+1, r);
+}
+
+void propagation(int i, int l, int r) {
+    if (lz[i]) {
+        if((r-l+1)%2==1) st[i] ^= lz[i];
+        if (l != r) {
+            lz[i*2] ^= lz[i];
+            lz[i*2+1] ^= lz[i];
+        }
+        lz[i] = 0;
+    }
+}
+
+ll update(int i, int l, int r, int s, int e, ll x) {
+    propagation(i, l, r);
+    if (e<l || r<s) return st[i];
+    if (s<=l && r<=e) {
+        lz[i] += x;
+        propagation(i, l, r);
+        return st[i];
+    }
+    int m=l+r>>1;
+    return st[i] = update(i*2, l, m, s, e, x)^update(i*2+1, m+1, r, s, e, x);
+}
+
+ll query(int i, int l, int r, int s, int e) {
+    propagation(i, l, r);
+    if (e<l || r<s) return 0;
+    if (s<=l && r<=e) return st[i];
+    int m=l+r>>1;
+    return query(i*2, l, m, s, e)^query(i*2+1, m+1, r, s, e);
+}
+
+int main() {
+    ios::sync_with_stdio(0); 
+    cin.tie(0); cout.tie(0);
+
+    int n;
+    cin >> n;
+    for(int i=1; i<=n; i++) {
+        cin >> an[i];
+    }
+    init(1, 1, n);
+
+    int m;
+    cin >> m;
+    for(int i=0; i<m; i++) {
+        ll a, b, c;
+        cin >> a >> b >> c;
+        b++;
+        c++;
+        if(a==1) {
+            ll d;
+            cin >> d;
+            update(1, 1, n, b, c, d);
+        } else {
+            cout << query(1, 1, n, b, c) << '\n';
+        }
+    }
+}
