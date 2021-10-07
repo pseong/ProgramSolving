1fb5d0abf6f76374c7e59cde753643e667c928ae
pseong <pkseonguk@gmail.com>
2021-10-07 12:31:36 +0900

    Modify 1517

--- Baekjoon Online Judge/1517.cpp
+++ Baekjoon Online Judge/1517.cpp
@@ -1,69 +1,60 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <string>
+#include <cstring>
+#include <cmath>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <tuple>
+#include <list>
+#include <map>
+#include <set>
 
 using namespace std;
+using ll = long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<long long, long long>;
+using tiii = tuple<int, int, int>;
+using tlll = tuple<long long, long long, long long>;
 
-typedef long long ll;
+pii an[1000010];
+ll st[4000010]{0};
 
-ll a[500010]{ 0 };
-ll sorted[500010]{ 0 };
-ll ans = 0;
-
-void merge(ll m, ll mid, ll n) {
-    ll i = m;
-    ll j = mid + 1;
-    ll k = m;
-
-    ll cnt = 0;
-
-    while(i <= mid && j <= n) {
-        if (a[i] <= a[j]) {
-            sorted[k] = a[i];
-            ans += cnt;
-            i++;
-        } else {
-            sorted[k] = a[j];
-            cnt++;
-            j++;
-        }
-        k++;
-    }   
-    if(i > mid) {
-        for(ll t = j; t <= n; t++) {
-            sorted[k] = a[t];
-            k++;
-        }
-    } else {
-        for(ll t = i; t <= mid; t++) {
-            sorted[k] = a[t];
-            ans += cnt;
-            k++;
-        }
-    }
-    for(ll t = m; t <= n; t++) {
-        a[t] = sorted[t];
-    }
+ll update(int i, int l, int r, int p, ll x) {
+    if (p<l || p>r) return st[i];
+    if (l==r) return st[i] = x;
+    int m=l+r>>1;
+    return st[i] = update(i*2, l, m, p, x)+update(i*2+1, m+1, r, p, x);
 }
 
-void mergeSort(ll m, ll n) {
-    if(m < n) {
-        ll mid = (m + n) / 2;
-        mergeSort(m, mid);
-        mergeSort(mid + 1, n);
-        merge(m, mid, n);
-    }
+ll query(int i, int l, int r, int s, int e) {
+    if(e<l || r<s) return 0;
+    if(s<=l && r<=e) return st[i];
+    int m=l+r>>1;
+    return query(i*2, l, m, s, e)+query(i*2+1, m+1, r, s, e);
 }
 
 int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+    ios::sync_with_stdio(0); 
+    cin.tie(0); cout.tie(0);
 
-    ll n;
+    int n;
     cin >> n;
-    for(ll i = 1; i <= n; i++) {
-        cin >> a[i];
+    for(int i=1; i<=n; i++) {
+        int a;
+        cin >> a;
+        an[i] = {a, i};
     }
+    sort(an+1, an+n+1);
 
-    mergeSort(1, n);
-
+    ll ans=0;
+    for(int i=1; i<=n; i++) {
+        int idx=an[i].second;
+        ans += query(1, 1, n, idx, n);
+        update(1, 1, n, idx, 1);
+    }
     cout << ans;
 }
