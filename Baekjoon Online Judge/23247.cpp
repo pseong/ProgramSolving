d533e01051ee4dcd8ae920a4c8f8a028ccd86c17
pseong <pkseonguk@gmail.com>
2021-10-12 02:26:37 +0900

    Add 23247

---
+++ Baekjoon Online Judge/23247.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <string>
+#include <cstring>
+#include <cmath>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <tuple>
+#include <list>
+#include <map>
+#include <set>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pdd = pair<double, double>;
+using pll = pair<long long, long long>;
+using tiii = tuple<int, int, int>;
+using tlll = tuple<long long, long long, long long>;
+
+int main() {
+    ios::sync_with_stdio(0); 
+    cin.tie(0); cout.tie(0);
+}
