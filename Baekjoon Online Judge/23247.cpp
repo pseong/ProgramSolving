121b2e5228cf14d3a4f53575c0acad7b4e2908e8
pseong <pkseonguk@gmail.com>
2021-10-12 02:58:47 +0900

    Modify 23247

--- Baekjoon Online Judge/23247.cpp
+++ Baekjoon Online Judge/23247.cpp
@@ -21,7 +21,42 @@ using pll = pair<long long, long long>;
 using tiii = tuple<int, int, int>;
 using tlll = tuple<long long, long long, long long>;
 
+ll sum[320][320]{0};
+int an[320][320]{0};
+
 int main() {
-    ios::sync_with_stdio(0); 
-    cin.tie(0); cout.tie(0);
+    int n, m;
+    cin >> n >> m;
+    for(int i=1; i<=n; i++) {
+        for(int j=1; j<=m; j++) {
+            cin >> an[i][j];
+        }
+    }
+
+    for(int i=1; i<=n; i++) {
+        for(int j=1; j<=m; j++) {
+            sum[i][j] = an[i][j];
+            sum[i][j] += sum[i-1][j];
+            sum[i][j] += sum[i][j-1];
+            sum[i][j] -= sum[i-1][j-1];
+        }
+    }
+    ll ans=0;
+    for(int x=1; x<=n; x++) {
+        for(int y=1; y<=m; y++) {
+            for(int i=x; i<=n; i++) {
+                bool fail=false;
+                for(int j=y; j<=m; j++) {
+                    ll res=sum[i][j];
+                    res -= sum[i][y-1];
+                    res -= sum[x-1][j];
+                    res += sum[x-1][y-1];
+                    if(res==10) ans++;
+                    else if(res>10) fail = true;
+                    if(fail) break;
+                }
+            }
+        }
+    }
+    cout << ans;
 }
