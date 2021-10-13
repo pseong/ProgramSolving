fffdc822669dc306c21e21d7fab4b84f50bbed79
pseong <pkseonguk@gmail.com>
2021-10-14 03:01:36 +0900

    Modify 0000

--- Baekjoon Online Judge/0000.cpp
+++ Baekjoon Online Judge/0000.cpp
@@ -1,7 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-typedef pair<ll,ll> pii;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
 #define pb push_back
 #define F first
 #define S second
