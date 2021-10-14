b9e09fd97c07fed056edb0c29846da01fedff971
pseong <pkseonguk@gmail.com>
2021-10-14 15:34:14 +0900

    Add 11689

---
+++ Baekjoon Online Judge/11689.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll=long long;
+using pii=pair<int,int>;
+using pll=pair<ll,ll>;
+#define pb push_back
+#define F first
+#define S second
+int main() {
+    ios::sync_with_stdio(0); 
+    cin.tie(0); cout.tie(0);
+
+    ll n;
+    cin >> n;
+    ll ans=n;
+    for(int i=2; i*i<=n; i++) {
+        if(n%i==0) {
+            ans -= ans/i;
+            while(n%i==0) n/= i;
+        }
+    }
+    if(n>1) ans -= ans/n;
+    cout << ans;
+}
