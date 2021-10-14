4180c44368e78a53cb2c596e0f708fa46237ec86
pseong <pkseonguk@gmail.com>
2021-10-15 03:10:42 +0900

    Modify 11438

--- Baekjoon Online Judge/11438.cpp
+++ Baekjoon Online Judge/11438.cpp
@@ -1,35 +1,21 @@
-#include <iostream>
-#include <iomanip>
-#include <algorithm>
-#include <string>
-#include <cstring>
-#include <cmath>
-#include <vector>
-#include <stack>
-#include <queue>
-#include <tuple>
-#include <map>
-#include <set>
-
+#include <bits/stdc++.h>
 using namespace std;
-using ll = long long;
-using ld = long double;
-using pii = pair<int, int>;
-using pll = pair<long long, long long>;
+using ll=long long;
+using pii=pair<int,int>;
+using pll=pair<ll,ll>;
+#define pb push_back
+#define F first
+#define S second
 
-int depth[100010]{0};
-int parent[100010][20]{0};
-int p_cost_min[100010][20]{0};
-int p_cost_max[100010][20]{0};
-vector<pii> v[100010];
-void dfs(int node, int d, int pre) {
-    depth[node] = d;
-    for(auto i : v[node]) {
-        if(i.first==pre) continue;
-        parent[i.first][0] = node;
-        p_cost_min[i.first][0] = i.second;
-        p_cost_max[i.first][0] = i.second;
-        dfs(i.first, d+1, node);
+int n, depth[510000], parent[510000][20];
+vector<int> v[510000];
+
+void dfs(int node, int par, int d) {
+    for(int i : v[node]) {
+        if(i==par) continue;
+        parent[i][0] = node;
+        depth[i] = d+1;
+        dfs(i, node, d+1);
     }
 }
 
@@ -37,21 +23,17 @@ int main() {
     ios::sync_with_stdio(0); 
     cin.tie(0); cout.tie(0);
 
-    int n;
     cin >> n;
-    for(int i=1; i<n; i++) {
-        int a, b, c;
-        cin >> a >> b >> c;
-        v[a].push_back({b, c});
-        v[b].push_back({a, c});
+    for(int i=0; i<n-1; i++) {
+        int a, b;
+        cin >> a >> b;
+        v[a].push_back(b);
+        v[b].push_back(a);
     }
     dfs(1, 0, 0);
-
-    for(int k=1; k<=19; k++) {
+    for(int k=1; k<20; k++) {
         for(int i=1; i<=n; i++) {
             parent[i][k] = parent[parent[i][k-1]][k-1];
-            p_cost_min[i][k] = min(p_cost_min[i][k-1], p_cost_min[parent[i][k-1]][k-1]);
-            p_cost_max[i][k] = max(p_cost_max[i][k-1], p_cost_max[parent[i][k-1]][k-1]);
         }
     }
 
@@ -60,29 +42,22 @@ int main() {
     for(int i=0; i<m; i++) {
         int a, b;
         cin >> a >> b;
-
-        int ans_min=1000000;
-        int ans_max=0;
         if(depth[a]<depth[b]) swap(a, b);
         for(int k=19; k>=0; k--) {
             if(depth[a]-depth[b]>=(1<<k)) {
-                ans_min = min(ans_min, p_cost_min[a][k]);
-                ans_max = max(ans_max, p_cost_max[a][k]);
                 a = parent[a][k];
             }
         }
-
         for(int k=19; k>=0; k--) {
             if(parent[a][k]!=parent[b][k]) {
-                ans_min = min(ans_min, p_cost_min[a][k]);
-                ans_min = min(ans_min, p_cost_min[b][k]);
-                ans_max = max(ans_max, p_cost_max[a][k]);
-                ans_max = max(ans_max, p_cost_max[b][k]);
                 a = parent[a][k];
                 b = parent[b][k];
             }
         }
-        if(a!=b) cout << min({ans_min, p_cost_min[a][0], p_cost_min[b][0]}) << ' ' << max({ans_max, p_cost_max[a][0], p_cost_max[b][0]}) << '\n';
-        else cout << ans_min << ' ' << ans_max << '\n';
+        if(a!=b) {
+            a = parent[a][0];
+            b = parent[b][0];
+        }
+        cout << a << '\n';
     }
 }
