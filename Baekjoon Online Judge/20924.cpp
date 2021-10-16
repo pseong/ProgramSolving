5743cdd173bc9f5ec103b87eacbdd14ec6509eb1
pseong <pkseonguk@gmail.com>
2021-10-16 22:57:16 +0900

    Add 20924

---
+++ Baekjoon Online Judge/20924.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll=long long;
+using pii=pair<int,int>;
+using pll=pair<ll,ll>;
+#define F first
+#define S second
+
+int n, r;
+vector<pii> v[200010];
+int ans1=-1;
+int ans2=-1;
+
+int dfs2(int node, int par, int dist) {
+    for(auto a : v[node]) {
+        if(a.first==par) continue;
+        ans2 = max(ans2, dist+a.second);
+        dfs2(a.first, node, dist+a.second);
+    }
+}
+
+int dfs(int node, int par, int dist) {
+    int cnt=0;
+    for(auto a : v[node]) {
+        if(a.first==par) continue;
+        cnt++;
+    }
+    if(cnt>1 && ans1==-1) {
+        ans1 = dist;
+        dfs2(node, par, 0);
+    }
+    else {
+        for(auto a : v[node]) {
+            if(a.first==par) continue;
+            dfs(a.first, node, dist+a.second);
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0); 
+    cin.tie(0); cout.tie(0);
+
+    cin >> n >> r;
+    for(int i=0; i<n-1; i++) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        v[a].push_back({b, c});
+        v[b].push_back({a, c});
+    }
+    dfs(r, 0, 0);
+    cout << ans1 << ' ' << ans2;
+}
