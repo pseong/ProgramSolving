2031e4fa30054bc96ba34021c618066894b0d8f5
pseong <pkseonguk@gmail.com>
2021-11-15 20:05:18 +0900

    Add 282A

---
+++ Codeforces/282A.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll=long long;
+using pii=pair<int,int>;
+using pll=pair<ll,ll>;
+#define F first
+#define S second
+int main() {
+    ios::sync_with_stdio(0); 
+    cin.tie(0); cout.tie(0);
+
+    int T;
+    cin >> T;
+    int ans=0;
+    while(T--) {
+        string s;
+        cin >> s;
+        if(s=="++X") {
+            ans++;
+        }
+        else if(s=="X++") {
+            ans++;
+        }
+        else if(s=="--X") {
+            ans--;
+        }
+        else if(s=="X--") {
+            ans--;
+        }
+    }
+    cout << ans;
+}
