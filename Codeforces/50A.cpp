3d25ec4bea3a79796d4abdd83f1679b21ff88886
pseong <pkseonguk@gmail.com>
2021-11-15 20:01:26 +0900

    Add 50A

---
+++ Codeforces/50A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll=long long;
+using pii=pair<int,int>;
+using pll=pair<ll,ll>;
+#define F first
+#define S second
+int main() {
+    ios::sync_with_stdio(0); 
+    cin.tie(0); cout.tie(0);
+
+    int m, n;
+    cin >> m >> n;
+    int a=m*n;
+    cout << a/2;
+}
