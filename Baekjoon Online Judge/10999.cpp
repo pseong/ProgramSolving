b5d526ef68f5fd2bdbdf527669ba62c4a40c155d
pseong <pkseonguk@gmail.com>
2022-03-16 05:54:08 +0900

    Fix 10999

--- Baekjoon Online Judge/10999.cpp
+++ Baekjoon Online Judge/10999.cpp
@@ -3,72 +3,81 @@ using namespace std;
 using ll=long long;
 using pii=pair<int,int>;
 using pll=pair<ll,ll>;
-#define pb push_back
 #define F first
 #define S second
 
-const int N = 1000000;
-int an[N+1];
-ll st[4*N];
-ll lz[4*N];
+using namespace std;
 
-ll init(int i, int l, int r) {
-    if (l==r) return st[i] = an[l];
-    int m=l+r>>1;
-    return st[i] = init(i*2, l, m)+init(i*2+1, m+1, r);
-}
+ll seg[4000010], lazy[4000010], an[1000010];
 
-void propagation(int i, int l, int r) {
-    if (lz[i]) {
-        st[i] += (r-l+1)*lz[i];
-        if (l != r) {
-            lz[i*2] += lz[i];
-            lz[i*2+1] += lz[i];
+void propagation(int node, int s, int e) {
+    if(lazy[node]) {
+        seg[node] += (e-s+1)*lazy[node];
+        if (s!=e) {
+            lazy[node*2] += lazy[node];
+            lazy[node*2+1] += lazy[node];
         }
-        lz[i] = 0;
+        lazy[node] = 0;
     }
 }
 
-ll update(int i, int l, int r, int s, int e, long long x) {
-    propagation(i, l, r);
-    if (e<l || r<s) return st[i];
-    if (s<=l && r<=e) {
-        lz[i] += x;
-        propagation(i, l, r);
-        return st[i];
+ll init(int node, int s, int e) {
+    if (s==e) {
+        seg[node] = an[s];
+        return seg[node];
     }
-    int m=l+r>>1;
-    return st[i] = update(i*2, l, m, s, e, x)+update(i*2+1, m+1, r, s, e, x);
+    int mid = (s + e) / 2;
+    seg[node] = init(node*2, s, mid) + init(node*2+1, mid+1, e);
+    return seg[node];
 }
 
-ll query(int i, int l, int r, int s, int e) {
-    propagation(i, l, r);
-    if (e<l || r<s) return 0;
-    if (s<=l && r<=e) return st[i];
-    int m=l+r>>1;
-    return query(i*2, l, m, s, e)+query(i*2+1, m+1, r, s, e);
+ll query(int node, int s, int e, int l, int r) {
+    propagation(node, s, e);
+    if (r < s || e < l) return 0;
+    if (l <= s && e <= r) return seg[node];
+    int mid = (s + e) / 2;
+    return query(node*2, s, mid, l, r) + query(node*2+1, mid+1, e, l, r);
+}
+
+ll update(int node, int s, int e, int l, int r, ll diff) {
+    propagation(node, s, e);
+    if (r < s || e < l) return seg[node];
+    if (l <= s && e <= r) {
+        lazy[node] += diff;
+        propagation(node, s, e);
+        return seg[node];
+    }
+    int mid = (s + e) / 2;
+    return seg[node] = update(node*2, s, mid, l, r, diff) + update(node*2+1, mid+1, e, l, r, diff);
 }
 
 int main() {
-    ios::sync_with_stdio(0); 
+    ios::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
 
-    ll n, m, k;
+    int n, m, k;
     cin >> n >> m >> k;
-    for(int i=1; i<=n; i++) {
+
+    for (int i=1; i<=n; i++) {
         cin >> an[i];
     }
+
     init(1, 1, n);
 
-    for(int i=0; i<m+k; i++) {
-        ll a, b, c;
-        cin >> a >> b >> c;
-        if(a==1) {
+    for (int i=0; i<m+k; i++) {
+        int a;
+        cin >> a;
+        if (a==1) {
+            int b, c;
             ll d;
-            cin >> d;
+            cin >> b >> c >> d;
             update(1, 1, n, b, c, d);
-        } else {
+        }
+        else if (a==2) {
+            int b, c;
+            cin >> b >> c;
             cout << query(1, 1, n, b, c) << '\n';
         }
     }
+    return 0;
 }
