e358f1381e248fbbbc1a81fe5968c362a58ada38
pseong <pkseonguk@gmail.com>
2022-03-16 22:39:20 +0900

    Fix default code

--- Baekjoon Online Judge/0000.cpp
+++ Baekjoon Online Judge/0000.cpp
@@ -1,10 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll=long long;
-using pii=pair<int,int>;
-using pll=pair<ll,ll>;
-#define F first
-#define S second
+
 int main() {
     ios::sync_with_stdio(0); 
     cin.tie(0); cout.tie(0);
