4be8f09cf092617b20254040fa540f6c21a8a9dd
pseong <pkseonguk@gmail.com>
2022-03-18 20:02:40 +0900

    Fix 13546

--- Baekjoon Online Judge/13546.cpp
+++ Baekjoon Online Judge/13546.cpp
@@ -2,66 +2,61 @@
 using namespace std;
 using ll=long long;
 
-int sqrtN;
+const int insz = 300;
+const int outsz = 350;
 
 struct Q {
     int idx, i, j;
-    bool operator < (Q& right) {
-        if (i != right.i) return i < right.i;
+    bool operator < (const Q& right) const {
+        if (i/insz != right.i/insz) return i < right.i;
         return j < right.j;
     }
 };
 
 int a[101010];
-int table[101010];
+int ans[101010];
+int bucket[outsz];
+int cnt[101010];
 vector<Q> query;
-deque<int> cnt[101010];
-int res;
+list<int> deq[101010];
 
-void AddFront(int x) {
+void Add(int x, int op) {
     int len = 0;
-    if (cnt[a[x]].size() >= 2) {
-        len = cnt[a[x]].back() - cnt[a[x]].front();
+    if (!deq[a[x]].empty()) {
+        len = deq[a[x]].back() - deq[a[x]].front();
+        cnt[len]--;
+        bucket[len/insz]--;
     }
-    table[len]--;
-    cnt[a[x]].push_front(x);
-    if (cnt[a[x]].size() >= 2) {
-        len = cnt[a[x]].back() - cnt[a[x]].front();
-    }
-    table[len]++;
-    res = max(res, len);
+    if (!op) deq[a[x]].push_front(x);
+    else deq[a[x]].push_back(x);
+    len = deq[a[x]].back() - deq[a[x]].front();
+    cnt[len]++;
+    bucket[len/insz]++;
 }
 
-void AddBack(int x) {
-    int len = 0;
-    if (cnt[a[x]].size() >= 2) {
-        len = cnt[a[x]].back() - cnt[a[x]].front();
+void Sub(int x, int op) {
+    int len = deq[a[x]].back() - deq[a[x]].front();
+    cnt[len]--;
+    bucket[len/insz]--;
+    if (!op) deq[a[x]].pop_front();
+    else deq[a[x]].pop_back();
+    if (!deq[a[x]].empty()) {
+        len = deq[a[x]].back() - deq[a[x]].front();
+        cnt[len]++;
+        bucket[len/insz]++;
     }
-    table[len]--;
-    cnt[a[x]].push_back(x);
-    if (cnt[a[x]].size() >= 2) {
-        len = cnt[a[x]].back() - cnt[a[x]].front();
-    }
-    table[len]++;
-    res = max(res, len);
 }
 
-void SubFront(int x) {
-    int len = 0;
-    if (cnt[a[x]].size() >= 2) {
-        len = cnt[a[x]].back() - cnt[a[x]].front();
+int Cal() {
+    for (int i=outsz-1; i>=0; i--) {
+        if (bucket[i] == 0) continue;
+        for (int j=insz-1; j>=0; j--) {
+            if (cnt[i*insz+j] > 0) {
+                return i*insz+j;
+            } 
+        }
     }
-    table[len]--;
-    cnt[a[x]].pop_front();
-    len = 0;
-    if (cnt[a[x]].size() >= 2) {
-        len = cnt[a[x]].back() - cnt[a[x]].front();
-    }
-    table[len]++;
-}
-
-void SubBack(int x) {
-
+    return 0;
 }
 
 int main() {
@@ -70,7 +65,6 @@ int main() {
 
     int n, k;
     cin >> n >> k;
-    sqrtN = sqrt(n);
     for (int i=1; i<=n; i++) {
         cin >> a[i];
     }
@@ -78,18 +72,28 @@ int main() {
     int m;
     cin >> m;
     for (int i=0; i<m; i++) {
-        int idx, x, y;
-        cin >> idx >> x >> y;
-        query.push_back({idx, x, y});
+        int x, y;
+        cin >> x >> y;
+        query.push_back({i, x, y});
     }
     sort(query.begin(), query.end());
 
-    int s = 0;
-    int e = 0;
-    for (auto q : query) {
-        while (s < q.i) SubFront(s++);
-        while (s > q.i) AddFront(--s);
-        while (e < q.j) AddBack(++e);
-        while (e > q.j) SubBack(e--);
+    int s = query[0].i;
+    int e = query[0].j;
+    for (int x=s; x<=e; x++) {
+        Add(x, 1);
+    }
+    ans[query[0].idx] = Cal();
+
+    for (int q=1; q<query.size(); q++) {
+        while (s > query[q].i) Add(--s, 0);
+        while (e < query[q].j) Add(++e, 1);
+        while (s < query[q].i) Sub(s++, 0);
+        while (e > query[q].j) Sub(e--, 1);
+        ans[query[q].idx] = Cal();
+    }
+
+    for (int i=0; i<m; i++) {
+        cout << ans[i] << '\n';
     }
 }
