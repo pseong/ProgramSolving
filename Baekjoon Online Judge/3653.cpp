0966d71c7ab52624b2f7ce206ede8c1063b447ee
pseong <pkseonguk@gmail.com>
2022-03-22 18:03:10 +0900

    Add 3653

--- Baekjoon Online Judge/3653.cpp
+++ Baekjoon Online Judge/3653.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 using ll=long long;
 
-int an[101010], n, m;
+int an[202020], n, m, idx[202020];
 struct Seg {
-    int tree[404040], lazy[404040];
+    int tree[808080], lazy[808080];
     
     void init(int node, int s, int e) {
         if (s==e) {
@@ -14,11 +14,12 @@ struct Seg {
         int mid = (s+e)/2;
         init(node*2, s, mid);
         init(node*2+1, mid+1, e);
+        tree[node] = tree[node*2] + tree[node*2+1];
     }
 
     void lazy_update(int node, int s, int e) {
         if (lazy[node]) {
-            tree[node] += (s+e-1)*lazy[node];
+            tree[node] += (e-s+1)*lazy[node];
             if (s!=e) {
                 tree[node*2] += lazy[node];
                 tree[node*2+1] += lazy[node];
@@ -29,7 +30,8 @@ struct Seg {
 
     void update(int node, int s, int e, int l, int r, int p) {
         lazy_update(node, s, e);
-        if (r<s || e<s) return;
+        if (r<l) return;
+        if (r<s || e<l) return;
         if (l<=s && e<=r) {
             lazy[node] += p;
             lazy_update(node, s, e);
@@ -41,12 +43,13 @@ struct Seg {
         tree[node] = tree[node*2] + tree[node*2+1];
     }
 
-    int query(int node, int s, int e, int x) {
+    int query(int node, int s, int e, int l, int r) {
         lazy_update(node, s, e);
-        if (x<s || x>e) return 0;
-        if (s==e) return tree[node];
+        if (r<l) return 0;
+        if (r<s || e<l) return 0;
+        if (l<=s && e<=r) return tree[node];
         int mid = (s+e)/2;
-        return query(node*2, s, mid, x) + query(node*2+1, mid+1, e, x);
+        return query(node*2, s, mid, l, r) + query(node*2+1, mid+1, e, l, r);
     }
 } seg;
 
@@ -56,17 +59,27 @@ int main() {
 
     int T; cin >> T;
     while (T--) {
+        memset(seg.lazy, 0, sizeof seg.lazy);
+        memset(seg.tree, 0, sizeof seg.tree);
+        memset(an, 0, sizeof an);
+        memset(idx, 0, sizeof idx);
+
         cin >> n >> m;
         for (int i=1; i<=n; i++) {
-            an[i] = n-i;
+            an[m+i] = 1;
+            idx[i] = m+i;
         }
-        memset(seg.lazy, 0, sizeof seg.lazy);
-        seg.init(1, 1, n);
+        seg.init(1, 1, n+m);
+
+        int space = m;
         for (int i=0; i<m; i++) {
             int x;
             cin >> x;
-            int ans = seg.query(1, 1, n, x);
-            cout << ans << ' ';
+            cout << seg.query(1, 1, n+m, 1, idx[x]-1) << ' ';
+            seg.update(1, 1, n+m, idx[x], idx[x], -1);
+            seg.update(1, 1, n+m, space, space, 1);
+            idx[x] = space;
+            space--;
         }
         cout << '\n';
     }
