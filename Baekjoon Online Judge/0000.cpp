90df2a6d5656c656efc247be64179092449618a0
pseong <pkseonguk@gmail.com>
2022-03-28 10:54:13 +0900

    Fix default code

--- Baekjoon Online Judge/0000.cpp
+++ Baekjoon Online Judge/0000.cpp
@@ -7,8 +7,8 @@ int dx[] = {-1, 1, 0, 0};
 int dy[] = {0, 0, -1, 1};
 
 int main() {
-    ios::sync_with_stdio(0); 
-    cin.tie(0); cout.tie(0);
+    ios::sync_with_stdio(false);
+    cin.tie(0);
 
     int T; cin >> T;
     while (T--) {
