ada5999d53c4604f9dac21c98e3efceff57a0dd9
pseong <pkseonguk@gmail.com>
2022-04-29 02:13:37 +0900

    Fix C

--- Topcoder/2022 TCO Algo/C.cpp
+++ Topcoder/2022 TCO Algo/C.cpp
@@ -7,33 +7,40 @@ using pll=pair<ll, ll>;
 class Stringversions {
     public:
     string create(int L, int N) {
-        int ans = 1;
+        int q = L / 26;
+        int r = L % 26;
+        string ans;
+        for (int i=25; i>=0; i--) {
+            for (int j=0; j<q; j++) {
+                ans.push_back('a'+i);
+            }
+            if (r > 0) {
+                ans.push_back('a'+i);
+                r--;
+            }
+        }
+
         int cnt = 0;
-        for (; ans<=26; ans++) {
-            if (ans*(ans-1)/2 >= N) break;
+        for (int i=0; i<ans.size(); i++) {
+            for (int j=i+1; j<ans.size(); j++) {
+                if (ans[i] > ans[j]) cnt++;
+            }
         }
-        ans = min(ans, 26);
-        for (;; cnt++) {
-            if (cnt*25 + ans*(ans-1)/2 >= N) break;
+        if (cnt < N) return "";
+        while (cnt != N) {
+            for (int i=0; i<ans.size()-1; i++) {
+                if (ans[i] > ans[i+1]) {
+                    swap(ans[i], ans[i+1]);
+                    cnt--;
+                    break;
+                }
+            }
         }
-        if (ans + cnt > L) return "";
-        int minus = cnt*25+ans*(ans-1)/2 - N;
-        vector<int> v;
-        for (int i=0; i<cnt; i++) v.push_back(25);
-        for (int i=ans-1; i>=0; i--) v.push_back(i);
-        while (v.size() < L) v.push_back(25);
-        string a;
-        for (int i=0; i<v.size(); i++) {
-            a.push_back(v[i]+'a');
-        }
-        for (int i=0; i<minus; i++) {
-            a[0]--;
-        }
-        return a;
+        return ans;
     }
 };
 
 int main() {
     Stringversions s;
-    cout << s.create(500, 120190);
+    cout << s.create(5, 1);
 }
