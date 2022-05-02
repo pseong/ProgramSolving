e242dfbb54d6e6c8c910a8d77a49cd4b3db1fb18
pseong <pkseonguk@gmail.com>
2022-05-02 20:39:41 +0900

    Add 18108

---
+++ Baekjoon Online Judge/18108.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll=long long;
+using pii=pair<int, int>;
+using pll=pair<ll, ll>;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int y;
+    cin >> y;
+    cout << y-(2541-1998);
+}
