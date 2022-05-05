56e6195f8f931d463347a622bd61ed48db18393a
pseong <pkseonguk@gmail.com>
2022-05-05 13:27:27 +0900

    Fix 22348

--- Baekjoon Online Judge/22348.cpp
+++ Baekjoon Online Judge/22348.cpp
@@ -1,33 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll=long long;
-using pii=pair<int,int>;
-using pll=pair<ll,ll>;
-#define F first
-#define S second
+using pii=pair<int, int>;
+using pll=pair<ll, ll>;
 
-const int MOD = 1000000007;
-int dp[510][50010];
-int prefix_dp[510][50010];
+int dp[505][50505];
+int predp[505][50505];
+const int mod = 1000000007;
 
 int main() {
-    ios::sync_with_stdio(0); 
-    cin.tie(0); cout.tie(0);
+    ios::sync_with_stdio(false);
+    cin.tie(0);
 
     dp[0][0] = 1;
-    for (int i=1; i<501; i++) {
-        for (int j=0; j<50001; j++) {
-            if (j+i <= 50001) {
-                dp[i][j+i] = (dp[i][j+i]+dp[i-1][j]) % MOD;
+    for (int i=1; i<=500; i++) {
+        for (int j=0; j<=50000; j++) {
+            dp[i][j] += dp[i-1][j];
+            dp[i][j] %= mod;
+            if (j-i >= 0) {
+                dp[i][j] += dp[i-1][j-i];
+                dp[i][j] %= mod;
             }
-            dp[i][j] = (dp[i][j]+dp[i-1][j]) % MOD;
         }
     }
 
-    for (int i=1; i<501; i++) {
-        prefix_dp[i][0] = dp[i][0];
-        for (int j=1; j<50001; j++) {
-            prefix_dp[i][j] = (prefix_dp[i][j-1]+dp[i][j]) % MOD;
+    for (int i=1; i<=500; i++) {
+        predp[i][0] = dp[i][0];
+        for (int j=1; j<=50000; j++) {
+            predp[i][j] = predp[i][j-1] + dp[i][j];
+            predp[i][j] %= mod;
         }
     }
 
@@ -35,17 +36,19 @@ int main() {
     while (T--) {
         int a, b;
         cin >> a >> b;
-        int ans=0;
-        for (int i=1; i<501; i++) {
+        int ans = 0;
+        for (int i=1; i<=500; i++) {
             int all = i*(i+1)/2;
-            if (all-b <= a) {
-                if (all-b-1 < 0) {
-                    ans = (ans+prefix_dp[i][a]) % MOD;
+            if (all <= a+b) {
+                if (all <= b) {
+                    ans += predp[i][a];
+                    ans %= mod;
                 }
                 else {
-                    int sum = prefix_dp[i][a]-prefix_dp[i][all-b-1];
-                    if (sum < 0) sum = MOD + sum;
-                    ans = (ans+sum) % MOD;
+                    int sum = predp[i][a] - predp[i][all-b-1];
+                    if (sum < 0) sum += mod;
+                    ans += sum;
+                    ans %= mod;
                 }
             }
         }
