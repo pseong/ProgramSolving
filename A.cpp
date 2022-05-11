fd1dcd199cab668f3722e980375c9ddd144ffb0b
pseong <pkseonguk@gmail.com>
2022-05-12 00:35:19 +0900

    Fix default code

--- A.cpp
+++ A.cpp
@@ -3,6 +3,7 @@ using namespace std;
 using ll=long long;
 using pi=pair<int, int>;
 using pll=pair<ll, ll>;
+ll gcd(ll a, ll b) { for (; b; a %= b, swap(a, b)); return a; }
 
 int main() {
     ios::sync_with_stdio(false);
