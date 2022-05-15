0b3e1c8452d887b37deaf7868ad632ab8891311f
pseong <pkseonguk@gmail.com>
2022-05-16 05:38:28 +0900

    Fix 2108

--- Baekjoon Online Judge/2108.cpp
+++ Baekjoon Online Judge/2108.cpp
@@ -1,59 +1,30 @@
-#include <iostream>
-#include <algorithm>
-#include <tuple>
-#include <vector>
-#include <cmath>
-#include <set>
-
+#include <bits/stdc++.h>
 using namespace std;
+using ll=long long;
+using pi=pair<int, int>;
+using pll=pair<ll, ll>;
+ll gcd(ll a, ll b) { for (; b; a %= b, swap(a, b)); return a; }
 
-int an[1000000]{0};
-
-int mn=1000000;
-int mx=-4000;
-int cnt[1000000]{0};
-
-bool func(pair<int, int> a, pair<int, int> b) {
-    if(a.first==b.first) {
-        return a.second > b.second;
-    }
-    return a.first < b.first;
+bool comp(pi a, pi b) {
+    if (a.first == b.first) return a.second < b.second;
+    return a.first > b.first;
 }
 int main() {
-    ios::sync_with_stdio(0); 
-    cin.tie(0); cout.tie(0);
-
-    int n;
-    cin >> n;
-    
-    set<int> set_an;
-    int sum=0;
-    for(int i=0; i<n; i++) {
-        cin >> an[i];
-        cnt[an[i]+4000]++;
-        set_an.insert(an[i]);
-        sum += an[i];
-        mx = max(mx, an[i]);
-        mn = min(mn, an[i]);
-    }
-    vector<pair<int, int>> v;
-    for(int zz : set_an) {
-        v.push_back({cnt[zz+4000], zz});
-    }
-    sort(an, an+n);
-    sort(v.begin(), v.end(), func);
-
-    int cb;
-    int a=v[v.size()-1].second;
-    int b= v[v.size()-2].second;
-    if(v.size()>1&&v[v.size()-1].first==v[v.size()-2].first) {
-        cb = v[v.size()-2].second;
-    } else {
-        cb = v[v.size()-1].second;
-    }
-
-    cout << round(double(sum)/n) << '\n';
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int n; cin >> n;
+    vector<int> an(n);
+    unordered_map<int, int> mp;
+    ll sum = 0;
+    for (int i=0; i<n; i++) cin >> an[i], mp[an[i]]++, sum += an[i];
+    cout << (int)round(double(sum)/n) << '\n';
+    sort(an.begin(), an.end());
     cout << an[n/2] << '\n';
-    cout << cb << '\n';
-    cout << mx - mn;
+    vector<pi> v;
+    for (auto at : mp) v.push_back({at.second, at.first});
+    sort(v.begin(), v.end(), comp);
+    if (v.size() > 1 && v[0].first == v[1].first)
+    cout << v[1].second << '\n';
+    else cout << v[0].second << '\n';
+    cout << an.back() - an.front() << '\n';
 }
