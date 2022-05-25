bc0310fedc932ec1a5a16b0c9a1077109ec17044
pseong <pkseonguk@gmail.com>
2022-05-25 22:17:04 +0900

    Fix C

--- Codeforces/Codeforces Round #793 (Div. 2)/C.cpp
+++ Codeforces/Codeforces Round #793 (Div. 2)/C.cpp
@@ -1,15 +1,12 @@
 #include <bits/stdc++.h>
-#define all(x) (x).begin(), (x).end()
 using namespace std;
-typedef long long ll;
-typedef pair<int, int> pii;
-typedef tuple<int, int, int> tp;
  
-int main(void) {
-	cin.tie(0)->sync_with_stdio(0);
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
  
-    int t; cin >> t;
-    while (t--) {
+    int T; cin >> T;
+    while (T--) {
         int n; cin >> n;
         map<int, int> mp;
         for (int i=0; i<n; i++) {
@@ -18,17 +15,17 @@ int main(void) {
         }
         int left = 0;
         int right = 0;
-        for (auto it = mp.begin(); it != mp.end(); it++) {
+        for (auto it = mp.rbegin(); it != mp.rend(); it++) {
             if (it->second >= 2) {
                 left++;
                 right++;
+                continue;
             }
             else {
                 if (left < right) left++;
-                else right++;
+                else right ++;
             }
         }
         cout << max(left, right) << '\n';
     }
-    return 0;
 }
