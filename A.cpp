09383f4da6b4d477d246079c7274fcbf387e1b79
pseong <pkseonguk@gmail.com>
2022-06-11 05:05:00 +0900

    Fix default Code

--- A.cpp
+++ A.cpp
@@ -1,8 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll=long long;
+using i128 = __int128_t
 using pi=pair<int, int>;
 using pll=pair<ll, ll>;
+using vi = vector<int>;
+using vll = vector<ll>;
+using vpi = vector<pi>;
+using vpll = vector<pll>;
+template <class T> using pq = priority_queue<T>;
+template <class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
 ll gcd(ll a, ll b) { for (; b; a %= b, swap(a, b)); return a; }
 
 int main() {
