687f0efc344345060da1a3a40ef7699b6a447545
pseong <pkseonguk@gmail.com>
2022-06-17 15:20:05 +0900

    Fix Educational Codeforces Round 130 (Rated for Div. 2) C

--- Codeforces/Educational Codeforces Round 130 (Rated for Div. 2)/C.cpp
+++ Codeforces/Educational Codeforces Round 130 (Rated for Div. 2)/C.cpp
@@ -13,6 +13,53 @@ using vpll = vector<pll>;
 template <class T> using pq = priority_queue<T>;
 template <class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
 ll gcd(ll a, ll b) { for (; b; a %= b, swap(a, b)); return a; }
+void no() { cout << "NO\n"; }
+void yes() { cout << "YES\n"; }
+
+void solve() {
+    int n; cin >> n;
+    string s, t;
+    cin >> s >> t;
+    for (auto c : {'a', 'b', 'c'}) {
+        if (count(s.begin(), s.end(), c) != count(t.begin(), t.end(), c)) {
+            no();
+            return;
+        }
+    }
+
+    string s1, t1;
+    vector<int> a, b;
+    for (int i=0; i<n; i++) {
+        if (s[i] != 'b') {
+            s1.push_back(s[i]);
+            a.push_back(i);
+        }
+    }
+    for (int i=0; i<n; i++) {
+        if (t[i] != 'b') {
+            t1.push_back(t[i]);
+            b.push_back(i);
+        }
+    }
+
+    if (s1 != t1) {
+        no();
+        return;
+    }
+
+    n = s1.size();
+    for (int i=0; i<n; i++) {
+        if (s1[i] == 'a' && a[i] > b[i]) {
+            no();
+            return;
+        }
+        else if (s1[i] == 'c' && a[i] < b[i]) {
+            no();
+            return;
+        }
+    }
+    yes();
+}
 
 int main() {
     ios::sync_with_stdio(false);
@@ -20,62 +67,6 @@ int main() {
 
     int T; cin >> T;
     while (T--) {
-        int n; cin >> n;
-        string s, t;
-        cin >> s >> t;
-        vector<int> scnt(3), tcnt(3);
-        for (int i=0; i<n; i++) {
-            scnt[s[i]-'a']++;
-        }
-        for (int i=0; i<n; i++) {
-            tcnt[t[i]-'a']++;
-        }
-        if (scnt[0] != tcnt[0] || scnt[1] != tcnt[1] || scnt[2] != tcnt[2]) {
-            cout << "NO\n";
-            continue;
-        }
-        bool ok = true;
-        for (int i=0; i<n; i++) {
-            if (s[i] != t[i]) {
-                bool f = false;
-                if (s[i] == 'a' && t[i] == 'b') {
-                    for (int k=i+1; k<n; k++) {
-                        if (s[k] == 'b') {
-                            swap(s[i], s[k]);
-                            f = true;
-                            break;
-                        }
-                        else if (s[k] != 'a') {
-                            ok = false;
-                            break;
-                        }
-                    }
-                }
-                else if (s[i] == 'b' && t[i] == 'c') {
-                    for (int k=i+1; k<n; k++) {
-                        if (s[k] == 'c') {
-                            swap(s[i], s[k]);
-                            f = true;
-                            break;
-                        }
-                        else if (s[k] != 'b') {
-                            ok = false;
-                            break;
-                        }
-                    }
-                }
-                else {
-                    ok = false;
-                    break;
-                }
-                if (!f) {
-                    ok = false;
-                    break;
-                }
-            }
-        }
-
-        if (ok) cout << "YES\n";
-        else cout << "NO\n";
+        solve();
     }
 }
