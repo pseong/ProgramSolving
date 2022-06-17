6c678269e68e424c094483449b1cd6feca6783b2
pseong <pkseonguk@gmail.com>
2022-06-17 15:14:09 +0900

    Fix defualt code

--- A.cpp
+++ A.cpp
@@ -1,5 +1,4 @@
 #include <bits/stdc++.h>
-#define int long long
 using namespace std;
 using ll=long long;
 using i128 = __int128_t;
@@ -7,15 +6,26 @@ using pi=pair<int, int>;
 using pll=pair<ll, ll>;
 using ti=tuple<int, int, int>;
 using tll=tuple<ll, ll, ll>;
+using vi = vector<int>;
+using vll = vector<ll>;
+using vpi = vector<pi>;
+using vpll = vector<pll>;
 template <class T> using pq = priority_queue<T>;
 template <class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
 ll gcd(ll a, ll b) { for (; b; a %= b, swap(a, b)); return a; }
+void no() { cout << "NO\n" << '\n'; }
+void yes() { cout << "YES\n" << '\n'; }
 
-signed main() {
+void solve() {
+    
+}
+
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
 
     int T; cin >> T;
     while (T--) {
+        solve();
     }
 }
