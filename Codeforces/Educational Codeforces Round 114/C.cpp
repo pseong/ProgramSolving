1952a830207931176fb16e166081658204626620
pseong <pkseonguk@gmail.com>
2022-06-23 04:51:42 +0900

    Fix Educational Codeforces Round 114 C

--- Codeforces/Educational Codeforces Round 114/C.cpp
+++ Codeforces/Educational Codeforces Round 114/C.cpp
@@ -1,65 +1,58 @@
-#include <iostream>
-#include <algorithm>
-
+#include <bits/stdc++.h>
+#define int long long
 using namespace std;
+using ll=long long;
+using i128 = __int128_t;
+using pi=pair<int, int>;
+using pll=pair<ll, ll>;
+using ti=tuple<int, int, int>;
+using tll=tuple<ll, ll, ll>;
+template <class T> using pq = priority_queue<T>;
+template <class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+ll gcd(ll a, ll b) { for (; b; a %= b, swap(a, b)); return a; }
+void no() { cout << "No" << '\n'; }
+void yes() { cout << "Yes" << '\n'; }
 
-int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
-
-    int n;
-    cin >> n;
-    int a[300]{0};
-    int sum=0;
-    for(int i=1; i<=n; i++) {
-        cin >> a[i];
-        sum += a[i];
+void solve() {
+    int n; cin >> n;
+    vector<int> v(n+1);
+    set<int> st;
+    int sum = 0;
+    for (int i=1; i<=n; i++) {
+        cin >> v[i];
+        sum += v[i];
+        st.insert(v[i]);
     }
-    sort(a+1, a+n+1);
-    int m;
-    cin >> m;
-    int x[300]{0}; // 방어력
-    int y[300]{0}; // 공격력
-    for(int i=1; i<=m; i++) {
-        cin >> x[i] >> y[i];
-    }
-
-    for(int i=1; i<=m; i++) {
-        int s=sum;
-        int ans=0;
-        int idx=lower_bound(a+1, a+n+1, x[i])-a;
-        if(idx>=n) {
-            idx--;
-            ans += x[i]-a[idx];
-            s -= a[idx];
-            if(s < y[i]) {
-                ans += y[i] - s;
-            }
-            cout << ans << '\n';
-            continue;
-        } else {
-            s -= a[idx];
-            if(s>=y[i]) {
-                cout << ans << '\n';
-                continue;
-            } else {
-                int ab=y[i]-s;
-                if(idx==1) {
-                    cout << ab << '\n';
-                    continue;
-                }
-                s += a[idx];
-                idx--;
-                int b=0;
-                if(a[idx] < x[i]) {
-                    b += x[i]-a[idx];
-                }
-                s -= a[idx];
-                if(s<y[i]) {
-                    b += y[i]-s;
-                }
-                cout << min(ab, b) << '\n';
-            }
+    int m; cin >> m;
+    while (m--) {
+        int x, y;
+        cin >> x >> y;
+        auto it = st.lower_bound(x);
+        if (it == st.end()) {
+            it--;
         }
+        int X = *it;
+        int Y = sum - X;
+        int gold = 0;
+        if (X < x) gold = gold += x - X;
+        if (Y < y) gold = gold += y - Y;
+
+        int gold2 = 0;
+        if (it != st.begin()) {
+            it--;
+            X = *it;
+            Y = sum - X;
+            if (X < x) gold2 = gold2 += x - X;
+            if (Y < y) gold2 = gold2 += y - Y;
+            gold = min(gold, gold2);
+        }
+        cout << gold << '\n';
     }
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    solve();
 }
