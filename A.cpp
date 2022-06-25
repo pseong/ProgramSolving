570b3a624bd61dac3d9e3f2ffefd16427fb6a40a
pseong <pkseonguk@gmail.com>
2022-06-25 12:54:51 +0900

    Fix defualt code

--- A.cpp
+++ A.cpp
@@ -1,5 +1,8 @@
 #include <bits/stdc++.h>
 #define int long long
+#define all(c) (c).begin(),(c).end()
+#define srt(c) sort(all(c))
+#define srtrev(c) sort(all(c)); reverse(all(c))
 using namespace std;
 using ll=long long;
 using i128 = __int128_t;
