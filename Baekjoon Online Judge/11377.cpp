1e2ae1b726f020c6d3c543a7b1af0d31562311b7
pseong <pkseonguk@gmail.com>
2022-07-10 04:12:43 +0900

    Add 11377

---
+++ Baekjoon Online Judge/11377.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+#define all(c) (c).begin(),(c).end()
+#define srt(c) sort(all(c))
+#define srtrev(c) sort(all(c)); reverse(all(c))
+using namespace std;
+using ll=long long;
+using i128 = __int128_t;
+using pi=pair<int, int>;
+using pll=pair<ll, ll>;
+using ti=tuple<int, int, int>;
+using tll=tuple<ll, ll, ll>;
+template <class T> using pq = priority_queue<T>;
+template <class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+ll gcd(ll a, ll b) { for (; b; a %= b, swap(a, b)); return a; }
+void no() { cout << "No" << '\n'; }
+void yes() { cout << "Yes" << '\n'; }
+
+vector<int> adj[1010];
+
+int c[1010], d[1010];
+
+bool dfs(int x) {
+    for (int y : adj[x]) {
+        if (c[y]) continue;
+        c[y] = true;
+        if (d[y] == 0 || dfs(d[y])) {
+            d[y] = x;
+            return true;
+        }
+    }
+    return false;
+}
+
+void solve() {
+    int n, m, k;
+    cin >> n >> m >> k;
+
+    for (int i=1; i<=n; i++) {
+        int cnt;
+        cin >> cnt;
+        while (cnt--) {
+            int a;
+            cin >> a;
+            adj[i].push_back(a);
+        }
+    }
+
+    int ans = 0;
+    for (int i=1; i<=n; i++) {
+        memset(c, 0, sizeof c);
+        if (dfs(i)) ans++;
+    }
+    for (int i=1; i<=n; i++) {
+        if (k == 0) break;
+        memset(c, 0, sizeof c);
+        if (dfs(i)) {
+            ans++;
+            k--;
+        }
+    }
+    cout << ans << '\n';
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    solve();
+}
