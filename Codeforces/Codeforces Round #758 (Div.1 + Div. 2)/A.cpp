05ce3bb9c9e6bf3cc42a10e57ccdf6f913be33dd
pseong <pkseonguk@gmail.com>
2022-07-15 04:34:50 +0900

    Fix A Codeforces Round #758 (Div.1 + Div. 2)

--- Codeforces/Codeforces Round #758 (Div.1 + Div. 2)/A.cpp
+++ Codeforces/Codeforces Round #758 (Div.1 + Div. 2)/A.cpp
@@ -15,14 +15,11 @@ ll gcd(ll a, ll b) { for (; b; a %= b, swap(a, b)); return a; }
 void no() { cout << "No" << '\n'; }
 void yes() { cout << "Yes" << '\n'; }
 
-vector<int> primes;
-int prime[1010101];
-
 void solve() {
     int n;
     cin >> n;
-    for (int i=1; i<=n; i++) {
-        cout << primes[i] << ' ';
+    for (int i=2; i<=n+1; i++) {
+        cout << i << ' ';
     }
     cout << '\n';
 }
@@ -31,16 +28,6 @@ signed main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
 
-    prime[0] = 1;
-    prime[1] = 1;
-    for (int i=2; i<=1000000; i++) {
-        if (prime[i] == 0) {
-            primes.push_back(i);
-            for (int j=i+i; j<=1000000; j+=i) {
-                prime[j] = 1;
-            }
-        }
-    }
     int T; cin >> T;
     while (T--) {
         solve();
