21684e50e80d46f11204cd94066f47a930cc4556
pseong <pkseonguk@gmail.com>
2022-07-16 20:53:22 +0900

    Add G Codeforces Round #806 (Div. 4)

---
+++ Codeforces/Codeforces Round #806 (Div. 4)/G.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#define all(c) (c).begin(),(c).end()
+#define srt(c) sort(all(c))
+#define srtrev(c) sort(all(c)); reverse(all(c))
+using namespace std;
+using ll=long long;
+using i128 = __int128_t;
+using pi=pair<int, int>;
+using pll=pair<ll, ll>;
+using ti=tuple<int, int, int>;
+using tll=tuple<ll, ll, ll>;
+template <class T> using pq = priority_queue<T>;
+template <class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+ll gcd(ll a, ll b) { for (; b; a %= b, swap(a, b)); return a; }
+void no() { cout << "No" << '\n'; }
+void yes() { cout << "Yes" << '\n'; }
+
+void solve() {
+    ll n, k;
+    cin >> n >> k;
+    vector<ll> v(n+1);
+    for (int i=1; i<=n; i++) {
+        cin >> v[i];
+    }
+    vector<vector<ll>> dp(n+1, vector<ll>(31));
+    dp[0][0] = 0;
+    ll ans = 0;
+    for (int i=1; i<=n; i++) {
+        dp[i][0] = dp[i-1][0] + v[i] - k;
+        for (int j=1; j<=30; j++) {
+            dp[i][j] = max({dp[i-1][j] + (v[i] >> j) - k, dp[i-1][j-1] + (v[i] >> j)});
+        }
+    }
+    for (int i=1; i<=n; i++) {
+        for (int j=0; j<=30; j++) {
+            ans = max(ans, dp[i][j]);
+        }
+    }
+    cout << ans << '\n';
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int T; cin >> T;
+    while (T--) {
+        solve();
+    }
+}
