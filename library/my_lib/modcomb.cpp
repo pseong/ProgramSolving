1985b2a182b2c6e303b980d829860dba56beb4dc
pseong <pkseonguk@gmail.com>
2022-07-18 22:40:00 +0900

    Add modcomb library

---
+++ library/my_lib/modcomb.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll=long long;
+
+const ll mod = 998244353;
+const ll MAX = 5050;
+ll fact[MAX], p[MAX];
+
+ll powll(int n, int r) {
+    if (r == 0) return 1;
+    if (r == 1) return n;
+    ll x = powll(n, r/2);
+    if (r%2 == 0) return x * x % mod;
+    else return x * x % mod * n % mod;
+}
+
+ll comb(int n, int r) {
+    return fact[n] * p[n-r] % mod * p[r] % mod;
+}
+
+void init() {
+    fact[0] = 1;
+    for (int i=1; i<MAX; i++) {
+        fact[i] = fact[i-1] * i % mod;
+    }
+    for (int i=0; i<MAX; i++) {
+        p[i] = powll(fact[i], mod-2);
+    }
+}
