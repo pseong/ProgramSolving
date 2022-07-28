4fd99c71a601d638bbfae5c1c5d301256ef1ffeb
pseong <pkseonguk@gmail.com>
2022-07-28 14:24:48 +0900

    Fix 11657

--- Baekjoon Online Judge/11657.cpp
+++ Baekjoon Online Judge/11657.cpp
@@ -1,50 +1,74 @@
 #include <bits/stdc++.h>
+#define all(c) (c).begin(),(c).end()
+#define srt(c) sort(all(c))
+#define srtrev(c) sort(all(c)); reverse(all(c))
 using namespace std;
 using ll=long long;
-using pii=pair<int,int>;
-using pll=pair<ll,ll>;
-#define F first
-#define S second
+using i128 = __int128_t;
+using pi=pair<int, int>;
+using pll=pair<ll, ll>;
+using ti=tuple<int, int, int>;
+using tll=tuple<ll, ll, ll>;
+template <class T> using pq = priority_queue<T>;
+template <class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+ll gcd(ll a, ll b) { for (; b; a %= b, swap(a, b)); return a; }
+void no() { cout << "No" << '\n'; }
+void yes() { cout << "Yes" << '\n'; }
 
-vector<tuple<int, int, int>> edge;
-int n, m;
-ll dist[510];
-const int inf = 1000000000;
+ll dist[550], cycle[550];
+bool inQ[550];
+const int inf = 1010101010;
+vector<pi> adj[550];
 
-int main() {
-    ios::sync_with_stdio(0); 
-    cin.tie(0); cout.tie(0);
-
+void solve(int CASE) {
+    int n, m;
     cin >> n >> m;
     for (int i=0; i<m; i++) {
-        int a, b, c;
-        cin >> a >> b >> c;
-        edge.push_back({a, b, c});
+        int u, v, w;
+        cin >> u >> v >> w;
+        adj[u].push_back({v, w});
     }
 
-    fill(dist, dist+n+1, inf);
+    for (int i=0; i<550; i++) {
+        dist[i] = inf;
+    }
 
-    bool cycle = false;
+    queue<int> q;
     dist[1] = 0;
-    for (int i=1; i<=n; i++) {
-        for (int j=0; j<edge.size(); j++) {
-            int from, to, cost;
-            tie(from, to, cost) = edge[j];
-            if (dist[from] != inf && dist[to] > dist[from] + cost) {
-                dist[to] = dist[from] + cost;
-                if (i==n) cycle = true;
+    cycle[1] = 1;
+    inQ[1] = true;
+    q.push(1);
+    while (q.size()) {
+        int x = q.front();
+        q.pop();
+        inQ[x] = false;
+        for (auto [y, w] : adj[x]) {
+            if (dist[y] > dist[x] + w) {
+                dist[y] = dist[x] + w;
+                cycle[y]++;
+                if (inQ[y]) continue;
+                if (cycle[y] >= n) {
+                    cout << -1 << '\n';
+                    return;
+                }
+                inQ[y] = true;
+                q.push(y);
             }
         }
     }
 
-    if (cycle) {
-        cout << -1 << '\n';
+    for (int i=2; i<=n; i++) {
+        if (dist[i] == inf) cout << -1 << '\n';
+        else cout << dist[i] << '\n';
     }
-    else {
-        for (int i=2; i<=n; i++) {
-            if (dist[i] == inf) dist[i] = -1;
-            cout << dist[i] << '\n';
-        }
+}
+
+int T = 1;
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    for (int t=1; t<=T; t++) {
+        solve(t);
     }
-    return 0;
 }
