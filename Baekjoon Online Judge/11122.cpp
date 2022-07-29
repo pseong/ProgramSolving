965c1a1c59d0c71c9f5875102696fba0cedb143f
pseong <pkseonguk@gmail.com>
2022-07-29 15:17:24 +0900

    Add 11122

---
+++ Baekjoon Online Judge/11122.cpp
@@ -0,0 +1,129 @@
+#include <bits/stdc++.h>
+#define all(c) (c).begin(),(c).end()
+#define srt(c) sort(all(c))
+#define srtrev(c) sort(all(c)); reverse(all(c))
+using namespace std;
+using ll=long long;
+using i128 = __int128_t;
+using pi=pair<int, int>;
+using pll=pair<ll, ll>;
+using ti=tuple<int, int, int>;
+using tll=tuple<ll, ll, ll>;
+template <class T> using pq = priority_queue<T>;
+template <class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+ll gcd(ll a, ll b) { for (; b; a %= b, swap(a, b)); return a; }
+void no() { cout << "No" << '\n'; }
+void yes() { cout << "Yes" << '\n'; }
+
+const int inf = 1010101010;
+struct MCMF {
+    struct Edge {
+        int y, c, f, w, rev;
+    };
+    vector<vector<Edge>> adj;
+    int n, s, t;
+    const int inf = 1010101010;
+
+    MCMF(int n, int s, int t) : n(n), s(s), t(t) {
+        adj.resize(n+1, vector<Edge>());
+    }
+
+    void addEdge(int x, int y, int c, int w) {
+        adj[x].push_back({y, c, 0, w, (int)adj[y].size()});
+        adj[y].push_back({x, 0, 0, -w, (int)adj[x].size()-1});
+    }
+
+    pll maxFlow() {
+        pll ans = {0, 0};
+        while (true) {
+            vector<int> dv(n+1, -1), didx(n+1, -1), inQ(n+1);
+            vector<ll> cost(n+1, inf);
+            queue<ll> q;
+            q.push(s);
+            inQ[s] = 1;
+            cost[s] = 0;
+            while (q.size()) {
+                ll x = q.front();
+                q.pop();
+                inQ[x] = 0;
+                for (int i=0; i<(int)adj[x].size(); i++) {
+                    Edge& e = adj[x][i];
+                    if (cost[e.y] > cost[x] + e.w && e.c - e.f > 0) {
+                        cost[e.y] = cost[x] + e.w;
+                        dv[e.y] = x;
+                        didx[e.y] = i;
+                        if (inQ[e.y]) continue;
+                        inQ[e.y] = 1;
+                        q.push(e.y);
+                    }
+                }
+            }
+            if (didx[t] == -1) break;
+            int flow = inf;
+            for (int x=t; x!=s; x=dv[x]) {
+                flow = min(flow, adj[dv[x]][didx[x]].c - adj[dv[x]][didx[x]].f);
+            }
+            for (ll x=t; x!=s; x=dv[x]) {
+                ans.second += (ll)adj[dv[x]][didx[x]].w * flow;
+                adj[dv[x]][didx[x]].f += flow;
+                adj[x][adj[dv[x]][didx[x]].rev].f -= flow;
+            }
+            ans.first += flow;
+        }
+        return ans;
+    }
+};
+
+void solve(int CASE) {
+    int n, p;
+    cin >> n >> p;
+    vector<vector<int>> a(n+1, vector<int>(n+1)), b(n+1, vector<int>(n+1));
+    for (int i=1; i<=n-1; i++) {
+        for (int j=i+1; j<=n; j++) {
+            cin >> a[i][j];
+        }
+    }
+    for (int i=1; i<=n-1; i++) {
+        for (int j=i+1; j<=n; j++) {
+            cin >> b[i][j];
+        }
+    }
+    vector<int> o(n+1);
+    for (int i=1; i<=n-1; i++) {
+        for (int j=i+1; j<=n; j++) {
+            int a;
+            cin >> a;
+            for (int k=i+1; k<=j; k++) {
+                o[k-1] += a;
+            }
+        }
+    }
+    MCMF mcmf(n+1, 0, n+1);
+    mcmf.addEdge(mcmf.s, 1, p, 0);
+    mcmf.addEdge(n, mcmf.t, p, 0);
+    for (int i=1; i<=n-1; i++) {
+        for (int j=i+1; j<=n; j++) {
+            mcmf.addEdge(i, j, b[i][j], -a[i][j]);
+        }
+    }
+    for (int i=1; i<=n-1; i++) {
+        mcmf.addEdge(i, i+1, inf, 0);
+    }
+    ll cnt = 0;
+    for (int i=1; i<=n-1; i++) {
+        mcmf.addEdge(i, i+1, o[i], -1000000);
+        cnt += o[i];
+    }
+    cout << -mcmf.maxFlow().second - cnt * 1000000 << '\n';
+}
+
+int T = 1;
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    cin >> T;
+    for (int t=1; t<=T; t++) {
+        solve(t);
+    }
+}
