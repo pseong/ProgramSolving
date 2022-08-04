7077a34eee19e88f3d89794b7f01171f241d529b
pseong <pkseonguk@gmail.com>
2022-08-04 17:14:36 +0900

    Add 24416

---
+++ Baekjoon Online Judge/24416.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#define all(c) (c).begin(),(c).end()
+#define srt(c) sort(all(c))
+#define srtrev(c) sort(all(c)); reverse(all(c))
+using namespace std;
+using ll=long long;
+using i128 = __int128_t;
+using pi=pair<int, int>;
+using pll=pair<ll, ll>;
+using ti=tuple<int, int, int>;
+using tll=tuple<ll, ll, ll>;
+template <class T> using pq = priority_queue<T>;
+template <class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+ll gcd(ll a, ll b) { for (; b; a %= b, swap(a, b)); return a; }
+void no() { cout << "No" << '\n'; }
+void yes() { cout << "Yes" << '\n'; }
+
+void solve(int CASE) {
+    int n;
+    cin >> n;
+    vector<ll> dp(n+1);
+    dp[1] = 1;
+    dp[2] = 1;
+    for (int i=3; i<=n; i++) {
+        dp[i] = dp[i-1] + dp[i-2];
+    }
+    cout << dp[n] << ' ' << n-2 << '\n';
+}
+
+int T = 1;
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    for (int t=1; t<=T; t++) {
+        solve(t);
+    }
+}
