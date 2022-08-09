6adb7e420ed376e42d385c0fb6f4d9211c962705
pseong <pkseonguk@gmail.com>
2022-08-10 03:17:32 +0900

    Fix C Codeforces Round #812 (Div. 2)

--- Codeforces/Codeforces Round #812 (Div. 2)/C.cpp
+++ Codeforces/Codeforces Round #812 (Div. 2)/C.cpp
@@ -15,30 +15,22 @@ ll gcd(ll a, ll b) { for (; b; a %= b, swap(a, b)); return a; }
 void no() { cout << "No" << '\n'; }
 void yes() { cout << "Yes" << '\n'; }
 
-int dp[101010];
-
 void solve(int CASE) {
     int n;
     cin >> n;
-    n--;
-    if (dp[n] == -1) {
-        cout << -1 << '\n';
-        return;
-    }
-    vector<int> ans(n+1);
-    for (int x=n; x>=0; x--) {
-        int y = x;
-        x = dp[y];
-        int l = x;
-        int r = y;
-        while (l <= r) {
-            ans[l] = r;
-            ans[r] = l;
-            l++;
-            r--;
+    vector<int> ans(n);
+    int cur = n-1;
+    int x = 0;
+    while (cur > x*x) x++;
+    while (cur > 0) {
+        while (cur <= (x-1)*(x-1)) x--;
+        int s = x*x - cur;
+        for (int i=0; i<=cur-s; i++) {
+            ans[s+i] = cur - i;
         }
+        cur = s - 1;
     }
-    for (int i=0; i<=n; i++) {
+    for (int i=0; i<n; i++) {
         cout << ans[i] << ' ';
     }
     cout << '\n';
@@ -49,25 +41,6 @@ signed main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
 
-    memset(dp, -1, sizeof dp);
-    dp[0] = 0;
-    dp[1] = 0;
-    for (int i=2; i<101010; i++) {
-        for (int x=0; x*x<101010; x++) {
-            if (i <= x*x) {
-                int df = x*x - i;
-                if (df > i) break;
-                if (df == i) {
-                    dp[i] = i;
-                    break;
-                }
-                if (dp[df-1] != -1) {
-                    dp[i] = df;
-                    break;
-                }
-            }
-        }
-    }
     cin >> T;
     for (int t=1; t<=T; t++) {
         solve(t);
