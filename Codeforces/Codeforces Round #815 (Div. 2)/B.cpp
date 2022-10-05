2a7263e233f203e3632421fbbaa91f7657b5c386
pseong <pkseonguk@gmail.com>
2022-10-05 21:02:01 +0900

    Add Codeforces Round #815 (Div. 2) B

---
+++ Codeforces/Codeforces Round #815 (Div. 2)/B.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#define all(c) (c).begin(),(c).end()
+#define srt(c) sort(all(c))
+#define srtrev(c) sort(all(c)); reverse(all(c))
+using namespace std;
+using ll=long long;
+using i128 = __int128_t;
+using pi=pair<int, int>;
+using pll=pair<ll, ll>;
+using ti=tuple<int, int, int>;
+using tll=tuple<ll, ll, ll>;
+template <class T> using pq = priority_queue<T>;
+template <class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+ll gcd(ll a, ll b) { for (; b; a %= b, swap(a, b)); return a; }
+void no() { cout << "No" << '\n'; }
+void yes() { cout << "Yes" << '\n'; }
+
+void solve(int CASE) {
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (int i=0; i<n; i++) {
+        cin >> v[i];
+    }
+    sort(v.begin(), v.end());
+    cout << v[n-1] + v[n-2] - v[0] - v[1] << '\n';
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int T = 1;
+    cin >> T;
+    for (int t=1; t<=T; t++) {
+        solve(t);
+    }
+}
