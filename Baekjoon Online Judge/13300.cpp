eb35d80f215a15b96712cb973b37b584de79381f
pseong <pkseonguk@gmail.com>
2022-10-17 19:36:42 +0900

    Add 13300

---
+++ Baekjoon Online Judge/13300.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#define all(c) (c).begin(),(c).end()
+#define srt(c) sort(all(c))
+#define srtrev(c) sort(all(c)); reverse(all(c))
+using namespace std;
+using ll=long long;
+using i128 = __int128_t;
+using pi=pair<int, int>;
+using pll=pair<ll, ll>;
+using ti=tuple<int, int, int>;
+using tll=tuple<ll, ll, ll>;
+template <class T> using pq = priority_queue<T>;
+template <class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+ll gcd(ll a, ll b) { for (; b; a %= b, swap(a, b)); return a; }
+void no() { cout << "No" << '\n'; }
+void yes() { cout << "Yes" << '\n'; }
+
+void solve(int CASE) {
+    int n, k;
+    cin >> n >> k;
+    vector<vector<int>> v(7, vector<int>(2, 0));
+    for (int i=0; i<n; i++) {
+        int a, b;
+        cin >> a >> b;
+        v[b][a]++;
+    }
+    int ans = 0;
+    for (int i=1; i<=6; i++) {
+        ans += (v[i][0] + k - 1) / k;
+        ans += (v[i][1] + k - 1) / k;
+    }
+    cout << ans << '\n';
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int T = 1;
+    for (int t=1; t<=T; t++) {
+        solve(t);
+    }
+}
