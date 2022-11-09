168e379e25f4a70364e4b99af023c3e957b16743
pseong <pkseonguk@gmail.com>
2022-11-09 15:15:18 +0900

    Add 9657

---
+++ Baekjoon Online Judge/9657.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define all(c) (c).begin(),(c).end()
+#define srt(c) sort(all(c))
+#define srtrev(c) sort(all(c)); reverse(all(c))
+using namespace std;
+using ll=long long;
+using i128 = __int128_t;
+using pi=pair<int, int>;
+using pll=pair<ll, ll>;
+using ti=tuple<int, int, int>;
+using tll=tuple<ll, ll, ll>;
+template <class T> using pq = priority_queue<T>;
+template <class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+ll gcd(ll a, ll b) { for (; b; a %= b, swap(a, b)); return a; }
+void no() { cout << "No" << '\n'; }
+void yes() { cout << "Yes" << '\n'; }
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+    vector<int> dp(n+1);
+    dp[1] = 1;
+    dp[3] = 1;
+    dp[4] = 1;
+    for (int i=5; i<=n; i++) {
+        if (!dp[i-1] || !dp[i-3] || !dp[i-4]) dp[i] = 1;
+    }
+    if (dp[n]) cout << "SK\n";
+    else cout << "CY\n";
+}
