a1ecf4a9f284c3d73b57eca28992962bd86594fa
pseong <pkseonguk@gmail.com>
2022-11-10 19:29:23 +0900

    Add 10610

---
+++ Baekjoon Online Judge/10610.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#define all(c) (c).begin(),(c).end()
+#define srt(c) sort(all(c))
+#define srtrev(c) sort(all(c)); reverse(all(c))
+using namespace std;
+using ll=long long;
+using i128 = __int128_t;
+using pi=pair<int, int>;
+using pll=pair<ll, ll>;
+using ti=tuple<int, int, int>;
+using tll=tuple<ll, ll, ll>;
+template <class T> using pq = priority_queue<T>;
+template <class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+ll gcd(ll a, ll b) { for (; b; a %= b, swap(a, b)); return a; }
+void no() { cout << "No" << '\n'; }
+void yes() { cout << "Yes" << '\n'; }
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    string s;
+    cin >> s;
+    int cnt = 0;
+    bool zero = false;
+    for (int i=0; i<(int)s.size(); i++) {
+        cnt += s[i] - '0';
+        if (s[i] == '0') zero = true;
+    }
+    if (cnt % 3 != 0 || !zero) {
+        cout << -1 << '\n';
+        return 0;
+    }
+    sort(s.rbegin(), s.rend());
+    cout << s << '\n';
+}
