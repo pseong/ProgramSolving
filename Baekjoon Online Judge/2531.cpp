35cf550acf49206d4b9d320218f0195c1a0084ea
pseong <pkseonguk@gmail.com>
2022-11-16 14:35:09 +0900

    Add 2531

---
+++ Baekjoon Online Judge/2531.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#define all(c) (c).begin(),(c).end()
+#define srt(c) sort(all(c))
+#define srtrev(c) sort(all(c)); reverse(all(c))
+using namespace std;
+using ll=long long;
+using i128 = __int128_t;
+using pi=pair<int, int>;
+using pll=pair<ll, ll>;
+using ti=tuple<int, int, int>;
+using tll=tuple<ll, ll, ll>;
+template <class T> using pq = priority_queue<T>;
+template <class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+ll gcd(ll a, ll b) { for (; b; a %= b, swap(a, b)); return a; }
+void no() { cout << "No" << '\n'; }
+void yes() { cout << "Yes" << '\n'; }
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n, d, k, c;
+    cin >> n >> d >> k >> c;
+    vector<int> v(n * 2), eat(d + 1);
+    for (int i=0; i<n; i++) {
+        cin >> v[i];
+        v[i + n] = v[i];
+    }
+    int cnt = 0;
+    int r = -1;
+    int ans = 0;
+    for (int l=0; l<n*2; l++) {
+        while (r < n*2 - 1 && r - l + 1 < k) {
+            if (++eat[v[++r]] == 1) cnt++;
+        }
+        ans = max(ans, cnt + (eat[c] == 0 ? 1 : 0));
+        if (--eat[v[l]] == 0) cnt--;
+    }
+    cout << ans << '\n';
+}
