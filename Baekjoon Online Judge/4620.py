6fe2fe1083cd5c7c7cf886ece55f710170fb3cb8
pseong <pkseonguk@gmail.com>
2022-11-29 02:15:04 +0900

    Add 4620

---
+++ Baekjoon Online Judge/4620.py
@@ -0,0 +1,18 @@
+
+while True:
+    n = int(input())
+    if n == -1: break
+    v = [[0 for i in range(n + 1)] for j in range(n + 1)]
+    v.append(0)
+    for i in range(1, n + 1):
+        tmp = input()
+        for j in range(1, n + 1):
+            v[i][j] = ord(tmp[j - 1]) - ord('0')
+    dp = [[0 for i in range(n + 1)] for j in range(n + 1)] 
+    dp[1][1] = 1
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            if v[i][j] == 0: continue
+            if i + v[i][j] <= n: dp[i + v[i][j]][j] += dp[i][j]
+            if j + v[i][j] <= n: dp[i][j + v[i][j]] += dp[i][j]
+    print(dp[n][n])
