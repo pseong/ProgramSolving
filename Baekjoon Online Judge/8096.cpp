c8a5d34dd08c1a79c4b91fee3738fb4ad427bab8
pseong <pkseonguk@gmail.com>
2022-12-01 18:59:12 +0900

    Add 8096

---
+++ Baekjoon Online Judge/8096.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#define all(c) (c).begin(),(c).end()
+#define srt(c) sort(all(c))
+#define srtrev(c) sort(all(c)); reverse(all(c))
+using namespace std;
+using ll=long long;
+using i128 = __int128_t;
+using pi=pair<int, int>;
+using pll=pair<ll, ll>;
+using ti=tuple<int, int, int>;
+using tll=tuple<ll, ll, ll>;
+template <class T> using pq = priority_queue<T>;
+template <class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+ll gcd(ll a, ll b) { for (; b; a %= b, swap(a, b)); return a; }
+void no() { cout << "No" << '\n'; }
+void yes() { cout << "Yes" << '\n'; }
+
+void solve(int CASE) {
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int n;
+    cin >> n;
+    vector<int> cnt(n + 1);
+    int m;
+    cin >> m;
+    for (int i=0; i<m; i++) {
+        int a, b;
+        cin >> a >> b;
+        cnt[a]++;
+        cnt[b]++;
+    }
+    ll sum = 0;
+    for (int i=1; i<=n; i++) {
+        sum += cnt[i]*(n - 1 - cnt[i]);
+    }
+    cout << n*(n-1)*(n-2)/6 - sum / 2 << '\n';
+}
