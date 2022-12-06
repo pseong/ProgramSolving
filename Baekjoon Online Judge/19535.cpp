da94ebeb456c7aa27a716eba872322b9cf61b5e6
pseong <pkseonguk@gmail.com>
2022-12-07 01:47:07 +0900

    Fix 19535

--- Baekjoon Online Judge/19535.cpp
+++ Baekjoon Online Judge/19535.cpp
@@ -1,39 +1,64 @@
 #include <bits/stdc++.h>
+#define all(c) (c).begin(),(c).end()
+#define srt(c) sort(all(c))
+#define srtrev(c) sort(all(c)); reverse(all(c))
 using namespace std;
 using ll=long long;
-using pii=pair<int,int>;
-using pll=pair<ll,ll>;
-#define F first
-#define S second
+using i128 = __int128_t;
+using pi=pair<int, int>;
+using pll=pair<ll, ll>;
+using ti=tuple<int, int, int>;
+using tll=tuple<ll, ll, ll>;
+template <class T> using pq = priority_queue<T>;
+template <class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+ll gcd(ll a, ll b) { for (; b; a %= b, swap(a, b)); return a; }
+void no() { cout << "No" << '\n'; }
+void yes() { cout << "Yes" << '\n'; }
 
-const int N=300000;
-vector<int> v[N+10];
-int vis[N+10];
-ll cnt;
+int n;
+vector<int> adj[303030];
+i128 dp[303030][5], dp2[303030][3];
 
-void dfs(int x, int parent) {
-    if(vis[x]) return;
-    vis[x] = 1;
-    int child=((x==1) ? 0 : -1);
-    child += v[x].size();
-    cnt += max(child-2, 0);
-    for(int y : v[x]) {
-        if(y==parent) continue;
-        dfs(y);
+void go(int x, int p) {
+    for (int y : adj[x]) {
+        if (y == p) continue;
+        go(y, x);
+        for (int i=2; i<=4; i++) {
+            dp[x][i] += dp[y][i-1];
+        }
+    }
+    i128 cnt = adj[x].size();
+    if (p != 0) cnt--;
+    if (cnt >= 2) dp2[x][1] = cnt * (cnt - 1) / 2;
+    if (cnt >= 3) dp2[x][0] = cnt * (cnt - 1) * (cnt - 2) / 6;
+    for (int y : adj[x]) {
+        if (y == p) continue;
+        dp2[x][0] += dp2[y][1];
+        dp2[x][2] += dp[y][2] * (cnt - 1);
     }
 }
 
-int main() {
-    ios::sync_with_stdio(0); 
-    cin.tie(0); cout.tie(0);
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
 
-    ll n;
     cin >> n;
-    for(int i=0; i<n-1; i++) {
+    for (int i=0; i<n-1; i++) {
         int a, b;
         cin >> a >> b;
-        v[a].push_back(b);
-        v[b].push_back(a);
+        adj[a].push_back(b);
+        adj[b].push_back(a);
     }
-    ll all=0;
+    for (int i=1; i<=n; i++) {
+        dp[i][1] = 1;
+    }
+    go(1, 0);
+    i128 d = 0, g = 0;
+    for (int i=1; i<=n; i++) {
+        d += dp[i][4] + dp2[i][2];
+        g += dp2[i][0];
+    }
+    if (d > g*3) cout << "D\n";
+    else if (d < g*3) cout << "G\n";
+    else cout << "DUDUDUNGA\n";
 }
