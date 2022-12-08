e5af15f28f62072509ebb5ae0c60f2f6ec7e46cb
pseong <pkseonguk@gmail.com>
2022-12-09 02:14:02 +0900

    Add 9463

---
+++ Baekjoon Online Judge/9463.cpp
@@ -0,0 +1,122 @@
+#include <bits/stdc++.h>
+#define all(c) (c).begin(),(c).end()
+#define srt(c) sort(all(c))
+#define srtrev(c) sort(all(c)); reverse(all(c))
+using namespace std;
+using ll=long long;
+using i128 = __int128_t;
+using pi=pair<int, int>;
+using pll=pair<ll, ll>;
+using ti=tuple<int, int, int>;
+using tll=tuple<ll, ll, ll>;
+template <class T> using pq = priority_queue<T>;
+template <class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+ll gcd(ll a, ll b) { for (; b; a %= b, swap(a, b)); return a; }
+void no() { cout << "No" << '\n'; }
+void yes() { cout << "Yes" << '\n'; }
+
+struct Lazyseg {
+	// EDIT TYPE
+	typedef struct {
+		long long sum;
+	} _T;
+	typedef long long _L;
+    //
+    _T(*op)(_T, _T);
+	int n;
+	vector<_T> S;
+	vector<_L> lazy;
+	_T t;
+	Lazyseg(_T(*op)(_T, _T), int n, _T t) : op(op), n(n), t(t) {
+		S.resize(4*n+10, {0});
+		lazy.resize(4*n+10, {0});
+	}
+	Lazyseg(_T(*op)(_T, _T), const vector<long long>& A, int n, _T t) : op(op), n(n), t(t) {
+		S.resize(4*n+10, {0});
+		lazy.resize(4*n+10, {0});
+		init(A, 1, 1, n);
+	}
+	_T init(const vector<long long>& A, int node, int s, int e) {
+		if (s == e) {
+			S[node].sum = A[s];
+			return S[node];
+		}
+		int mid = (s+e)/2;
+		return S[node] = op(init(A, node*2, s, mid), init(A, node*2+1, mid+1, e));
+	}
+	void propagate(int node, int s, int e) {
+		if (lazy[node] == 0) return;
+        // EDIT HERE
+        S[node].sum += ((long long)e-s+1)*lazy[node];
+        if (s!=e) {
+            lazy[node*2] += lazy[node];
+            lazy[node*2+1] += lazy[node];
+        }
+        //
+		lazy[node] = 0;
+	}
+	_T query(int node, int s, int e, int l, int r) {
+        propagate(node, s, e);
+		if (e < l || r < s) return t;
+		if (l <= s && e <= r) return S[node];
+		int mid = (s+e)/2;
+		return op(query(node*2, s, mid, l, r), query(node*2+1, mid+1, e, l, r));
+	}
+	_T query(int l, int r) {
+		return query(1, 1, n, l, r);
+	}
+	void update_range(int node, int s, int e, int l, int r, _L x) {
+        propagate(node, s, e);
+		if (e < l || r < s) return;
+		if (l <= s && e <= r) {
+			// EDIT HERE
+			lazy[node] += x;
+            //
+			propagate(node, s, e);
+			return;
+		}
+		int mid = (s + e) / 2;
+		update_range(node*2, s, mid, l, r, x);
+		update_range(node*2+1, mid+1, e, l, r, x);
+		S[node] = op(S[node*2], S[node*2+1]);
+	}
+	void update_range(int l, int r, _L x) {
+		update_range(1, 1, n, l, r, x);
+	}
+};
+
+Lazyseg::_T op(Lazyseg::_T a, Lazyseg::_T b) {
+    return { a.sum + b.sum };
+}
+
+void solve(int CASE) {
+    int n;
+    cin >> n;
+    vector<int> v(n + 1), idx(n + 1);
+    for (int i=1; i<=n; i++) {
+        int a;
+        cin >> a;
+        idx[a] = i;
+    }
+    for (int i=1; i<=n; i++) {
+        cin >> v[i];
+    }
+    Lazyseg seg(op, n, { 0 });
+    ll ans = 0;
+    for (int i=1; i<=n; i++) {
+        ans += seg.query(idx[v[i]], idx[v[i]]).sum;
+        seg.update_range(1, idx[v[i]], 1);
+    }
+    cout << ans << '\n';
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int T = 1;
+    cin >> T;
+    for (int t=1; t<=T; t++) {
+        solve(t);
+    }
+}
