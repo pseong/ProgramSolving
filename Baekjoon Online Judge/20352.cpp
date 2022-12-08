e006cfcbf669be63064a559997d75e234b4d84da
pseong <pkseonguk@gmail.com>
2022-12-08 13:42:23 +0900

    Add 20352

---
+++ Baekjoon Online Judge/20352.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define all(c) (c).begin(),(c).end()
+#define srt(c) sort(all(c))
+#define srtrev(c) sort(all(c)); reverse(all(c))
+using namespace std;
+using ll=long long;
+using i128 = __int128_t;
+using pi=pair<int, int>;
+using pll=pair<ll, ll>;
+using ti=tuple<int, int, int>;
+using tll=tuple<ll, ll, ll>;
+template <class T> using pq = priority_queue<T>;
+template <class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+ll gcd(ll a, ll b) { for (; b; a %= b, swap(a, b)); return a; }
+void no() { cout << "No" << '\n'; }
+void yes() { cout << "Yes" << '\n'; }
+
+const double PI = atan(1) * 4;
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    double n;
+    cin >> n;
+    cout << fixed << setprecision(6) << sqrt(n / PI) * 2 * PI;
+}
