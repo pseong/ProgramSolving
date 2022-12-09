69bc0e2b0198cbae3f4e426afad5528336ccbf42
pseong <pkseonguk@gmail.com>
2022-12-09 13:08:18 +0900

    Add 21314

---
+++ Baekjoon Online Judge/21314.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+#define all(c) (c).begin(),(c).end()
+#define srt(c) sort(all(c))
+#define srtrev(c) sort(all(c)); reverse(all(c))
+using namespace std;
+using ll=long long;
+using i128 = __int128_t;
+using pi=pair<int, int>;
+using pll=pair<ll, ll>;
+using ti=tuple<int, int, int>;
+using tll=tuple<ll, ll, ll>;
+template <class T> using pq = priority_queue<T>;
+template <class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+ll gcd(ll a, ll b) { for (; b; a %= b, swap(a, b)); return a; }
+void no() { cout << "No" << '\n'; }
+void yes() { cout << "Yes" << '\n'; }
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    string s;
+    cin >> s;
+    int n = s.size();
+    string mx, mn;
+    int cnt = 0;
+    for (int i=0; i<n; i++) {
+        if (s[i] == 'M') cnt++;
+        else if (s[i] == 'K') {
+            mx.push_back('5');
+            mx += string(cnt, '0');
+            cnt = 0;
+        }
+    }
+    mx += string(cnt, '1');
+    cnt = 0;
+    for (int i=0; i<n; i++) {
+        if (s[i] == 'M') cnt++;
+        else if (s[i] == 'K') {
+            if (cnt) {
+                mn.push_back('1');
+                mn += string(cnt - 1, '0');
+            }
+            mn.push_back('5');
+            cnt = 0;
+        }
+    }
+    if (cnt) {
+        mn.push_back('1');
+        mn += string(cnt - 1, '0');
+    }
+    cout << mx << '\n' << mn << '\n';
+}
