624efd6ad3f07b5d3e5270f5f78203686199cd87
pseong <pkseonguk@gmail.com>
2022-12-09 18:39:28 +0900

    Add 2141

---
+++ Baekjoon Online Judge/2141.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#define all(c) (c).begin(),(c).end()
+#define srt(c) sort(all(c))
+#define srtrev(c) sort(all(c)); reverse(all(c))
+using namespace std;
+using ll=long long;
+using i128 = __int128_t;
+using pi=pair<int, int>;
+using pll=pair<ll, ll>;
+using ti=tuple<int, int, int>;
+using tll=tuple<ll, ll, ll>;
+template <class T> using pq = priority_queue<T>;
+template <class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+ll gcd(ll a, ll b) { for (; b; a %= b, swap(a, b)); return a; }
+void no() { cout << "No" << '\n'; }
+void yes() { cout << "Yes" << '\n'; }
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int n;
+    cin >> n;
+    vector<pll> v(n);
+    ll sum = 0;
+    for (int i=0; i<n; i++) {
+        cin >> v[i].first >> v[i].second;
+        sum += v[i].second;
+    }
+    sort(v.begin(), v.end());
+    ll l = 0;
+    ll r = sum;
+    for (int i=0; i<n; i++) {
+        r += v[i].second;
+    }
+    for (int i=0; i<n; i++) {
+        r -= v[i].second;
+        l += v[i].second;
+        if (l * 2 >= sum) {
+            cout << v[i].first << '\n';
+            return 0;
+        }
+    }
+}
