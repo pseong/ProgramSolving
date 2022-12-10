737b8d39ef1d2b11aaa3375fe5ee01fe9790cfe5
pseong <pkseonguk@gmail.com>
2022-12-11 02:21:15 +0900

    Add 2670

---
+++ Baekjoon Online Judge/2670.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#define all(c) (c).begin(),(c).end()
+#define srt(c) sort(all(c))
+#define srtrev(c) sort(all(c)); reverse(all(c))
+using namespace std;
+using ll=long long;
+using i128 = __int128_t;
+using pi=pair<int, int>;
+using pll=pair<ll, ll>;
+using ti=tuple<int, int, int>;
+using tll=tuple<ll, ll, ll>;
+template <class T> using pq = priority_queue<T>;
+template <class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+ll gcd(ll a, ll b) { for (; b; a %= b, swap(a, b)); return a; }
+void no() { cout << "No" << '\n'; }
+void yes() { cout << "Yes" << '\n'; }
+
+double dp[10101];
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int n;
+    cin >> n;
+    vector<double> v(n + 1);
+    for (int i=1; i<=n; i++) {
+        cin >> v[i];
+    }
+    double ans = 0;
+    for (int i=1; i<=n; i++) {
+        dp[i] = max(v[i], v[i] * dp[i-1]);
+        ans = max(ans, dp[i]);
+    }
+    cout << fixed << setprecision(3) << ans << '\n';
+}
