735e932db292459a22497a43ebf7b18297364a9f
pseong <pkseonguk@gmail.com>
2022-12-11 02:39:32 +0900

    Add 17212

---
+++ Baekjoon Online Judge/17212.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#define all(c) (c).begin(),(c).end()
+#define srt(c) sort(all(c))
+#define srtrev(c) sort(all(c)); reverse(all(c))
+using namespace std;
+using ll=long long;
+using i128 = __int128_t;
+using pi=pair<int, int>;
+using pll=pair<ll, ll>;
+using ti=tuple<int, int, int>;
+using tll=tuple<ll, ll, ll>;
+template <class T> using pq = priority_queue<T>;
+template <class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+ll gcd(ll a, ll b) { for (; b; a %= b, swap(a, b)); return a; }
+void no() { cout << "No" << '\n'; }
+void yes() { cout << "Yes" << '\n'; }
+
+ll dp[101010];
+const ll inf = 1e18;
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int n;
+    cin >> n;
+    fill(dp, dp + 101010, inf);
+    dp[0] = 0;
+    for (int x : {1, 2, 5, 7}) {
+        for (int i=0; i<=100000; i++) {
+            if (i + x <= 100000) dp[i + x] = min(dp[i + x], dp[i] + 1);
+        }
+    }
+    cout << dp[n] << '\n';
+}
