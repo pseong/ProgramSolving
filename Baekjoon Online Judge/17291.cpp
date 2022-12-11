92dc60d8e0cc5b0175e283cd0f26f1fcd2b98c3c
pseong <pkseonguk@gmail.com>
2022-12-11 23:52:44 +0900

    Add 17291

---
+++ Baekjoon Online Judge/17291.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#define all(c) (c).begin(),(c).end()
+#define srt(c) sort(all(c))
+#define srtrev(c) sort(all(c)); reverse(all(c))
+using namespace std;
+using ll=long long;
+using i128 = __int128_t;
+using pi=pair<int, int>;
+using pll=pair<ll, ll>;
+using ti=tuple<int, int, int>;
+using tll=tuple<ll, ll, ll>;
+template <class T> using pq = priority_queue<T>;
+template <class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+ll gcd(ll a, ll b) { for (; b; a %= b, swap(a, b)); return a; }
+void no() { cout << "No" << '\n'; }
+void yes() { cout << "Yes" << '\n'; }
+
+int dp[22][5];
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int n;
+    cin >> n;
+    dp[1][3] = 1;
+    for (int i=2; i<=n; i++) {
+        if (i % 2 == 1) {
+            dp[i][3] += dp[i-1][1] + dp[i-1][2] + dp[i-1][3] + dp[i-1][4];
+        }
+        else {
+            dp[i][4] += dp[i-1][1] + dp[i-1][2] + dp[i-1][3] + dp[i-1][4];
+        }
+        dp[i][1] += dp[i-1][2];
+        dp[i][2] += dp[i-1][3];
+        dp[i][3] += dp[i-1][4];
+    }
+    int ans = 0;
+    for (int i=1; i<=4; i++) {
+        ans += dp[n][i];
+    }
+    cout << ans << '\n';
+}
