248ca7589dacfb542f31222a3f2810c551c0c6c4
pseong <pkseonguk@gmail.com>
2022-12-12 19:56:17 +0900

    Add 14567

---
+++ Baekjoon Online Judge/14567.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#define all(c) (c).begin(),(c).end()
+#define srt(c) sort(all(c))
+#define srtrev(c) sort(all(c)); reverse(all(c))
+using namespace std;
+using ll=long long;
+using i128 = __int128_t;
+using pi=pair<int, int>;
+using pll=pair<ll, ll>;
+using ti=tuple<int, int, int>;
+using tll=tuple<ll, ll, ll>;
+template <class T> using pq = priority_queue<T>;
+template <class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+ll gcd(ll a, ll b) { for (; b; a %= b, swap(a, b)); return a; }
+void no() { cout << "No" << '\n'; }
+void yes() { cout << "Yes" << '\n'; }
+
+int in[1010], dp[1010];
+vector<int> adj[1010];
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int n, m;
+    cin >> n >> m;
+    for (int i=0; i<m; i++) {
+        int a, b;
+        cin >> a >> b;
+        in[b]++;
+        adj[a].push_back(b);
+    }
+    queue<int> q;
+    for (int i=1; i<=n; i++) {
+        if (in[i] == 0) q.push(i);
+    }
+    while (q.size()) {
+        int x = q.front();
+        q.pop();
+        for (int y : adj[x]) {
+            dp[y] = max(dp[y], dp[x] + 1);
+            if (--in[y] == 0) q.push(y);
+        }
+    }
+    for (int i=1; i<=n; i++) {
+        cout << dp[i] + 1 << ' ';
+    }   
+}
