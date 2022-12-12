84105868cf0611b1662010f4499998e71ac6a58e
pseong <pkseonguk@gmail.com>
2022-12-12 21:08:11 +0900

    Add 2758

---
+++ Baekjoon Online Judge/2758.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#define all(c) (c).begin(),(c).end()
+#define srt(c) sort(all(c))
+#define srtrev(c) sort(all(c)); reverse(all(c))
+using namespace std;
+using ll=long long;
+using i128 = __int128_t;
+using pi=pair<int, int>;
+using pll=pair<ll, ll>;
+using ti=tuple<int, int, int>;
+using tll=tuple<ll, ll, ll>;
+template <class T> using pq = priority_queue<T>;
+template <class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+ll gcd(ll a, ll b) { for (; b; a %= b, swap(a, b)); return a; }
+void no() { cout << "No" << '\n'; }
+void yes() { cout << "Yes" << '\n'; }
+
+ll dp[15][2020];
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        for (int i=0; i<=n; i++) {
+            for (int j=0; j<=m; j++) {
+                dp[i][j] = 0;
+            }
+        }
+        dp[0][0] = 1;
+        for (int i=1; i<=n; i++) {
+            for (int j=1; j<=m; j++) {
+                for (int k=0; k<=j/2; k++) {
+                    dp[i][j] += dp[i-1][k];
+                }
+            }
+        }
+        ll ans = 0;
+        for (int j=0; j<=m; j++) {
+            ans += dp[n][j];
+        }
+        cout << ans << '\n';
+    }
+}
