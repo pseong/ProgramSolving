c183ba9025f246e746b208150ba7a65f88e41b7d
pseong <pkseonguk@gmail.com>
2022-12-13 03:53:49 +0900

    Add 3687

--- Baekjoon Online Judge/3687.cpp
+++ Baekjoon Online Judge/3687.cpp
@@ -15,29 +15,20 @@ ll gcd(ll a, ll b) { for (; b; a %= b, swap(a, b)); return a; }
 void no() { cout << "No" << '\n'; }
 void yes() { cout << "Yes" << '\n'; }
 
-string dp[110];
-vector<string> v{"", "", "1", "7", "4", "2", "0", "8"};
-string inf = "999999999999999";
+ll inf = 999999999999999;
+ll dp[110]{0, inf, 1, 7, 4, 2, 6, 8};
+vector<ll> v{0, 0, 1, 7, 4, 2, 0, 8};
 
 signed main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
 
-    dp[0] = "";
-    for (int i=1; i<=100; i++) {
+    for (int i=8; i<=100; i++) {
         dp[i] = inf;
     }
-    for (int i=2; i<=100; i++) {
+    for (int i=8; i<=100; i++) {
         for (int j=2; j<=7; j++) {
-            if (i-j < 0) continue;
-            string a = dp[i-j] + v[j];
-            string b = v[j] + dp[i-j];
-            if (j == 6) {
-                a = dp[i-j] + "0";
-                if (stoll(a) == 0) a = inf;
-                b = "6" + dp[i-j];
-            }
-            dp[i] = to_string(min({stoll(dp[i]), stoll(a), stoll(b)}));
+            dp[i] = min(dp[i], dp[i-j] * 10 + v[j]);
         }
     }
     int t;
