e3be73ef418c206e3282b8bb862f7ad0fa222c5d
pseong <pkseonguk@gmail.com>
2022-12-14 20:05:26 +0900

    Add 2411

---
+++ Baekjoon Online Judge/2411.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+#define all(c) (c).begin(),(c).end()
+#define srt(c) sort(all(c))
+#define srtrev(c) sort(all(c)); reverse(all(c))
+using namespace std;
+using ll=long long;
+using i128 = __int128_t;
+using pi=pair<int, int>;
+using pll=pair<ll, ll>;
+using ti=tuple<int, int, int>;
+using tll=tuple<ll, ll, ll>;
+template <class T> using pq = priority_queue<T>;
+template <class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+ll gcd(ll a, ll b) { for (; b; a %= b, swap(a, b)); return a; }
+void no() { cout << "No" << '\n'; }
+void yes() { cout << "Yes" << '\n'; }
+
+ll dp[110][110], v[110][110], s[110][2];
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int n, m, a, b;
+    cin >> n >> m >> a >> b;
+    vector<pi> p(a);
+    for (int i=0; i<a; i++) {
+        cin >> p[i].first >> p[i].second;
+    }
+    p.push_back({n, m});
+    sort(p.begin(), p.end());
+    for (int i=0; i<b; i++) {
+        int x, y;
+        cin >> x >> y;
+        v[x][y] = 1;
+    }
+    ll ans = 1;
+    pi now = {1, 1};
+    for (auto nxt : p) {
+        dp[now.first][now.second] = 1;
+        for (int i=now.first; i<=nxt.first; i++) {
+            for (int j=now.second; j<=nxt.second; j++) {
+                if (v[i][j]) continue;
+                dp[i][j] += dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        ans *= dp[nxt.first][nxt.second];
+        for (int i=now.first; i<=nxt.first; i++) {
+            for (int j=now.second; j<=nxt.second; j++) {
+                dp[i][j] = 0;
+            }
+        }
+        now = nxt;
+    }
+    cout << ans << '\n';
+}
