144abef12a85fc285809c50584b2ff7ef1764b48
pseong <pkseonguk@gmail.com>
2022-12-19 04:00:36 +0900

    Add 15806

---
+++ Baekjoon Online Judge/15806.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+#define all(c) (c).begin(),(c).end()
+#define srt(c) sort(all(c))
+#define srtrev(c) sort(all(c)); reverse(all(c))
+using namespace std;
+using ll=long long;
+using i128 = __int128_t;
+using pi=pair<int, int>;
+using pll=pair<ll, ll>;
+using ti=tuple<int, int, int>;
+using tll=tuple<ll, ll, ll>;
+template <class T> using pq = priority_queue<T>;
+template <class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+ll gcd(ll a, ll b) { for (; b; a %= b, swap(a, b)); return a; }
+void no() { cout << "No" << '\n'; }
+void yes() { cout << "Yes" << '\n'; }
+
+int v[330][330][2], chk[2020][2020];
+int dx[] {-1, -2, -2, -1, 1, 2, 2, 1};
+int dy[] {-2, -1, 1, 2, 2, 1, -1, -2};
+
+int dist(pi a, pi b) {
+    return abs(a.first - b.first) + abs(a.second - b.second);
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n, m, k, t;
+    cin >> n >> m >> k >> t;
+    
+    queue<ti> q;
+    for (int i=0; i<m; i++) {
+        int a, b;
+        cin >> a >> b;
+        bool ok = false;
+        for (int d=0; d<8; d++) {
+            int x = a + dx[d];
+            int y = b + dy[d];
+            if (x < 1 || x > n || y < 1 || y > n) continue;
+            ok = true;
+        }
+        if (ok) {
+            v[a][b][0] = 1;
+            q.push({a, b, 0});
+        }
+    }
+
+    for (int tt=1; tt<=t; tt++) {
+        int ttt = q.size();
+        for (int T=1; T<=ttt; T++) {
+            auto [x, y, c] = q.front();
+            q.pop();
+            for (int d=0; d<8; d++) {
+                int a = x + dx[d];
+                int b = y + dy[d];
+                if (a < 1 || a > n || b < 1 || b > n) continue;
+                if (v[a][b][c^1]) continue;
+                v[a][b][c^1] = 1;
+                q.push({a, b, c^1});
+            }
+        }
+    }
+    bool ok = false;
+    for (int i=0; i<k; i++) {
+        int a, b;
+        cin >> a >> b;
+        if (v[a][b][t % 2]) ok = true;
+    }
+    if (ok) cout << "YES\n";
+    else cout << "NO\n";
+}
