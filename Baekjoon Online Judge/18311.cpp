74f4c6e762bedd9ce77527fa2cbc7a1266cb2ab8
pseong <pkseonguk@gmail.com>
2022-12-19 17:51:43 +0900

    Fix 18311

--- Baekjoon Online Judge/18311.cpp
+++ Baekjoon Online Judge/18311.cpp
@@ -7,7 +7,7 @@ using ll=long long;
 using i128 = __int128_t;
 using pi=pair<int, int>;
 using pll=pair<ll, ll>;
-using ti=tuple<int, int, int>;
+using ti=tuple<int, int, int>
 using tll=tuple<ll, ll, ll>;
 template <class T> using pq = priority_queue<T>;
 template <class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
@@ -15,7 +15,7 @@ ll gcd(ll a, ll b) { for (; b; a %= b, swap(a, b)); return a; }
 void no() { cout << "No" << '\n'; }
 void yes() { cout << "Yes" << '\n'; }
 
-ll n, k, v[202];
+ll n, k, v[202020];
 
 signed main() {
     ios::sync_with_stdio(false);
