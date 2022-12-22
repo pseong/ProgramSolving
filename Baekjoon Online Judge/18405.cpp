60d048217e51fe1f732970d3e89cef704cf6559a
pseong <pkseonguk@gmail.com>
2022-12-23 01:12:19 +0900

    Add 18405

---
+++ Baekjoon Online Judge/18405.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+#define all(c) (c).begin(),(c).end()
+#define srt(c) sort(all(c))
+#define srtrev(c) sort(all(c)); reverse(all(c))
+using namespace std;
+using ll=long long;
+using i128 = __int128_t;
+using pi=pair<int, int>;
+using pll=pair<ll, ll>;
+using ti=tuple<int, int, int>;
+using tll=tuple<ll, ll, ll>;
+template <class T> using pq = priority_queue<T>;
+template <class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+ll gcd(ll a, ll b) { for (; b; a %= b, swap(a, b)); return a; }
+void no() { cout << "No" << '\n'; }
+void yes() { cout << "Yes" << '\n'; }
+
+int v[220][220];
+vector<pi> lst[1010];
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int n, k;
+    cin >> n >> k;
+    for (int i=1; i<=n; i++) {
+        for (int j=1; j<=n; j++) {
+            cin >> v[i][j];
+            if (v[i][j]) lst[v[i][j]].push_back({i, j});
+        }
+    }
+    queue<pi> q;
+    for (int i=1; i<=k; i++) {
+        for (pi p : lst[i]) {
+            q.push(p);
+        }
+    }
+    int s, r, c;
+    cin >> s >> r >> c;
+    while (s--) {
+        int t = q.size();
+        while (t--) {
+            auto [x, y] = q.front();
+            q.pop();
+            int dx[] {-1, 1, 0, 0};
+            int dy[] {0, 0, -1, 1};
+            for (int d=0; d<4; d++) {
+                int a = x + dx[d];
+                int b = y + dy[d];
+                if (a < 1 || a > n || b < 1 || b > n) continue;
+                if (v[a][b]) continue;
+                v[a][b] = v[x][y];
+                q.push({a, b});
+            }
+        }
+    }
+    cout << v[r][c] << '\n';
+}
