a35ac42501e5e5f0f60511fbefa0b012979266f7
pseong <pkseonguk@gmail.com>
2022-12-27 03:13:20 +0900

    Add 13410

---
+++ Baekjoon Online Judge/13410.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define all(c) (c).begin(),(c).end()
+#define srt(c) sort(all(c))
+#define srtrev(c) sort(all(c)); reverse(all(c))
+using namespace std;
+using ll=long long;
+using i128 = __int128_t;
+using pi=pair<int, int>;
+using pll=pair<ll, ll>;
+using ti=tuple<int, int, int>;
+using tll=tuple<ll, ll, ll>;
+template <class T> using pq = priority_queue<T>;
+template <class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+ll gcd(ll a, ll b) { for (; b; a %= b, swap(a, b)); return a; }
+void no() { cout << "No" << '\n'; }
+void yes() { cout << "Yes" << '\n'; }
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int n, k;
+    cin >> n >> k;
+    int ans = 0;
+    for (int i=1; i<=k; i++) {
+        int x = i * n;
+        string s = to_string(x);
+        reverse(s.begin(), s.end());
+        int a = stoi(s);
+        ans = max(ans, a);
+    }
+    cout << ans << '\n';
+}
