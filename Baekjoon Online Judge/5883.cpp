9a6340a4bf176c615d917a4b76a8122e3e772ba4
pseong <pkseonguk@gmail.com>
2022-12-27 16:30:45 +0900

    Add 5883

---
+++ Baekjoon Online Judge/5883.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+#define all(c) (c).begin(),(c).end()
+#define srt(c) sort(all(c))
+#define srtrev(c) sort(all(c)); reverse(all(c))
+using namespace std;
+using ll=long long;
+using i128 = __int128_t;
+using pi=pair<int, int>;
+using pll=pair<ll, ll>;
+using ti=tuple<int, int, int>;
+using tll=tuple<ll, ll, ll>;
+template <class T> using pq = priority_queue<T>;
+template <class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+ll gcd(ll a, ll b) { for (; b; a %= b, swap(a, b)); return a; }
+void no() { cout << "No" << '\n'; }
+void yes() { cout << "Yes" << '\n'; }
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    set<int> st;
+    for (int i=0; i<n; i++) {
+        cin >> v[i];
+        st.insert(v[i]);
+    }
+    int ans = 0;
+    for (int x : st) {
+        vector<int> tmp;
+        for (int i=0; i<n; i++) {
+            if (v[i] != x) tmp.emplace_back(v[i]);
+        }
+        int prev = 0;
+        for (int i=1; i<=(int)tmp.size(); i++) {
+            if (i == tmp.size() || tmp[i] != tmp[i-1]) {
+                ans = max(ans, i - prev);
+                prev = i;
+            }
+        }
+    }
+    cout << ans << '\n';
+}
