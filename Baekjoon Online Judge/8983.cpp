ce7ae059a3fdcf0616203c2a461b4222cb60fc2a
pseong <pkseonguk@gmail.com>
2022-12-29 18:27:39 +0900

    Add 8983

---
+++ Baekjoon Online Judge/8983.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#define all(c) (c).begin(),(c).end()
+#define srt(c) sort(all(c))
+#define srtrev(c) sort(all(c)); reverse(all(c))
+using namespace std;
+using ll=long long;
+using i128 = __int128_t;
+using pi=pair<int, int>;
+using pll=pair<ll, ll>;
+using ti=tuple<int, int, int>;
+using tll=tuple<ll, ll, ll>;
+template <class T> using pq = priority_queue<T>;
+template <class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+ll gcd(ll a, ll b) { for (; b; a %= b, swap(a, b)); return a; }
+void no() { cout << "No" << '\n'; }
+void yes() { cout << "Yes" << '\n'; }
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int m, n, l;
+    cin >> m >> n >> l;
+    vector<int> v(m);
+    for (int i=0; i<m; i++) {
+        cin >> v[i];
+    }
+    sort(v.begin(), v.end());
+    int ans = 0;
+    for (int i=0; i<n; i++) {
+        int x, y;
+        cin >> x >> y;
+        if (y > l) continue;
+        int lo = x - (l - y);
+        int hi = x + (l - y);
+        auto it = lower_bound(v.begin(), v.end(), lo);
+        if (it != v.end() && *it >= lo && *it <= hi) ans++;
+    }
+    cout << ans << '\n';
+}
