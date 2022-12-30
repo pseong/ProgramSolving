2e08dfd96916a14693b22724b866a1acfaec3c4a
pseong <pkseonguk@gmail.com>
2022-12-30 17:58:09 +0900

    Add 1189

---
+++ Baekjoon Online Judge/1189.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#define all(c) (c).begin(),(c).end()
+#define srt(c) sort(all(c))
+#define srtrev(c) sort(all(c)); reverse(all(c))
+using namespace std;
+using ll=long long;
+using i128 = __int128_t;
+using pi=pair<int, int>;
+using pll=pair<ll, ll>;
+using ti=tuple<int, int, int>;
+using tll=tuple<ll, ll, ll>;
+template <class T> using pq = priority_queue<T>;
+template <class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+ll gcd(ll a, ll b) { for (; b; a %= b, swap(a, b)); return a; }
+void no() { cout << "No" << '\n'; }
+void yes() { cout << "Yes" << '\n'; }
+
+int n, m, k, ans, chk[10][10];
+string v[10];
+int dx[] {-1, 1, 0, 0};
+int dy[] {0, 0, -1, 1};
+
+void go(int x, int y, int cnt) {
+    if (cnt == k) {
+        if (x == 0 && y == m - 1) ans++;
+        return;
+    }
+    for (int d=0; d<4; d++) {
+        int a = x + dx[d];
+        int b = y + dy[d];
+        if (a < 0 || a >= n || b < 0 || b >= m) continue;
+        if (chk[a][b] || v[a][b] == 'T') continue;
+        chk[a][b] = 1;
+        go(a, b, cnt + 1);
+        chk[a][b] = 0;
+    }
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    cin >> n >> m >> k;
+    for (int i=0; i<n; i++) {
+        cin >> v[i];
+    }
+    chk[n-1][0] = 1;
+    go(n-1, 0, 1);
+    cout << ans << '\n';
+}
