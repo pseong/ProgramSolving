c76015340a8616185a7f4eeb3b344096e38945d3
pseong <pkseonguk@gmail.com>
2023-01-01 03:02:09 +0900

    Fix 18430

--- Baekjoon Online Judge/18430.cpp
+++ Baekjoon Online Judge/18430.cpp
@@ -17,7 +17,7 @@ void yes() { cout << "Yes" << '\n'; }
 
 int n, m, ans, now, v[10][10], chk[10][10];
 
-bool ok(int x, int y) { 
+bool ok(int x, int y) {
     if (x < 0 || x >= n || y < 0 || y >= m) return 1;
     else return chk[x][y];
 }
@@ -58,57 +58,16 @@ void go(int x, int y) {
     }
 }
 
-void go2(int x, int y) {
-    ans = max(ans, now);
-    if (y == m) {
-        y = 0;
-        x++;
-        if (x == n) return;
-    }
-    go(x, y + 1);
-    vector<tuple<int, int, int, int, int, int>> lst;
-    lst.push_back({x, y, x + 1, y, x, y + 1});
-    lst.push_back({x, y + 1, x, y, x + 1, y + 1});
-    lst.push_back({x + 1, y + 1, x + 1, y, x, y + 1});
-    lst.push_back({x, y + 1, x, y, x + 1, y + 1});
-    for (auto [x, y, a, b, r, c] : lst) {
-        if (!ok(x, y) && !ok(a, b) && !ok(r, c)) {
-            open(x, y, a, b, r, c);
-            go(x, y + 1);
-            close(x, y, a, b, r, c);
-        }
-    }
-}
-
 signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
 
-    for (int k=0; k<1000000; k++) {
-        cout << '.';
-        // cout << "==========================================\n";
-        n = rand() % 5 + 1;
-        m = rand() % 5 + 1;
-        for (int i=0; i<n; i++) {
-            for (int j=0; j<m; j++) {
-                v[i][j] = rand() % 100 + 1;
-            }
-        }
-        ans = 0;
-        go(0, 0);
-        int x = ans;
-        ans = 0;
-        go2(0, 0);
-        int y = ans;
-        // cout << x << ' ' << y << ' ' << (x == y) << '\n';
-        if (x != y) {
-            cout << "==========================================\n";
-            cout << n << ' ' << m << '\n';
-            for (int i=0; i<n; i++) {
-                for (int j=0; j<m; j++) {
-                    cout << v[i][j] << ' ';
-                }
-                cout << '\n';
-            }
-            cout << "==========================================\n";
+    cin >> n >> m;
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<m; j++) {
+            cin >> v[i][j];
         }
     }
+    go(0, 0);
+    cout << ans << '\n';
 }
