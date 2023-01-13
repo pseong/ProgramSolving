ace9b5f965f715abbac27e80a9e9e629600db069
pseong <pkseonguk@gmail.com>
2023-01-13 23:55:54 +0900

    Add A1 A2 B C Codeforces Round #843 (Div. 2)

---
+++ Codeforces/Codeforces Round #843 (Div. 2)/C.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#define all(c) (c).begin(),(c).end()
+#define srt(c) sort(all(c))
+#define srtrev(c) sort(all(c)); reverse(all(c))
+using namespace std;
+using ll=long long;
+using i128 = __int128_t;
+using pi=pair<int, int>;
+using pll=pair<ll, ll>;
+using ti=tuple<int, int, int>;
+using tll=tuple<ll, ll, ll>;
+template <class T> using pq = priority_queue<T>;
+template <class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+ll gcd(ll a, ll b) { for (; b; a %= b, swap(a, b)); return a; }
+void no() { cout << "No" << '\n'; }
+void yes() { cout << "Yes" << '\n'; }
+
+void solve(int CASE) {
+    ll n, x;
+    cin >> n >> x;
+    if (n == x) {
+        cout << n << '\n';
+        return;
+    }
+    for (int i=59; i>=0; i--) {
+        if ((n & (1LL<<i)) && !(x & (1LL<<i))) {
+            ll N = n;
+            for (int j=i; j>=0; j--) {
+                N |= (1LL<<j);
+            }
+            N++;
+            ll y = n & N;
+            if ((n & N) != x) cout << -1 << '\n';
+            else cout << N << '\n';
+            return;
+        }
+    }
+    cout << -1 << '\n';
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int T = 1;
+    cin >> T;
+    for (int t=1; t<=T; t++) {
+        solve(t);
+    }
+}
