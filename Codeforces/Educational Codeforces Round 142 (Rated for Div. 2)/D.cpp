cb78bf13fd9f12204b637193166f6e7a41945027
pseong <pkseonguk@gmail.com>
2023-01-25 02:00:24 +0900

    Add A B C D Educational Codeforces Round 142 (Rated for Div. 2)

---
+++ Codeforces/Educational Codeforces Round 142 (Rated for Div. 2)/D.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+#define all(c) (c).begin(),(c).end()
+#define srt(c) sort(all(c))
+#define srtrev(c) sort(all(c)); reverse(all(c))
+using namespace std;
+using ll=long long;
+using i128 = __int128_t;
+using pi=pair<int, int>;
+using pll=pair<ll, ll>;
+using ti=tuple<int, int, int>;
+using tll=tuple<ll, ll, ll>;
+template <class T> using pq = priority_queue<T>;
+template <class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+ll gcd(ll a, ll b) { for (; b; a %= b, swap(a, b)); return a; }
+void no() { cout << "No" << '\n'; }
+void yes() { cout << "Yes" << '\n'; }
+
+void solve(int CASE) {
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>> v(n + 1, vector<int>(m + 1));
+    vector<vector<int>> v2(n + 1, vector<int>(m + 1));
+    vector<int> chk(m + 1);
+    set<vector<int>> st;
+    for (int i=1; i<=n; i++) {
+        for (int j=1; j<=m; j++) {
+            cin >> v[i][j];
+            chk[v[i][j]] = j;
+        }
+        for (int j=1; j<=m; j++) {
+            v2[i][j] = chk[j];
+        }
+        vector<int> tmp;
+        for (int j=1; j<=m; j++) {
+            tmp.push_back(v2[i][j]);
+            st.insert(tmp);
+        }
+    }
+    for (int i=1; i<=n; i++) {
+        vector<int> tmp;
+        int ans = 0;
+        for (int j=1; j<=m; j++) {
+            tmp.push_back(v[i][j]);
+            if (st.find(tmp) == st.end()) break;
+            ans++;
+        }
+        cout << ans << ' ';
+    }
+    cout << '\n';
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int T = 1;
+    cin >> T;
+    for (int t=1; t<=T; t++) {
+        solve(t);
+    }
+}
