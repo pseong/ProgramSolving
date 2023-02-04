17cd75b8c7b6c999e78128c2e4d6f89655a6fe14
pseong <pkseonguk@gmail.com>
2023-02-04 11:30:47 +0900

    Fix B Codeforces Global Round 24

--- Codeforces/Codeforces Global Round 24/B.cpp
+++ Codeforces/Codeforces Global Round 24/B.cpp
@@ -18,16 +18,13 @@ void yes() { cout << "Yes" << '\n'; }
 void solve(int CASE) {
     int n;
     cin >> n;
-    vector<int> v(n);
-    for (int i=0; i<n; i++) cin >> v[i];
-    vector<int> v2(n-1);
-    for (int i=1; i<n; i++) {
-        v2[i-1] = gcd(v[0], v[i]);
-    }
     int g = 0;
-    for (int i=0; i<n-1; i++) g = gcd(g, v2[i]);
-    if (g == 1) cout << *max_element(v.begin(), v.end()) << '\n';
-    else cout << *max_element(v.begin(), v.end()) / g << '\n';
+    vector<int> v(n);
+    for (int i=0; i<n; i++) {
+        cin >> v[i];
+        g = gcd(g, v[i]);
+    }
+    cout << v[n-1] / g << '\n';
 }
 
 signed main() {
