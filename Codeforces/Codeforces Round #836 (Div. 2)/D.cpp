4efbf1a8bebeabef164619a0a4b5028908fbf47c
pseong <pkseonguk@gmail.com>
2023-02-14 15:20:17 +0900

    Add A B C D Codeforces Round #836 (Div. 2)

---
+++ Codeforces/Codeforces Round #836 (Div. 2)/D.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+#define all(c) (c).begin(),(c).end()
+#define srt(c) sort(all(c))
+#define srtrev(c) sort(all(c)); reverse(all(c))
+using namespace std;
+using ll=long long;
+using i128 = __int128_t;
+using pi=pair<int, int>;
+using pll=pair<ll, ll>;
+using ti=tuple<int, int, int>;
+using tll=tuple<ll, ll, ll>;
+template <class T> using pq = priority_queue<T>;
+template <class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+ll gcd(ll a, ll b) { for (; b; a %= b, swap(a, b)); return a; }
+void no() { cout << "No" << '\n'; }
+void yes() { cout << "Yes" << '\n'; }
+
+void solve(int CASE) {
+    int n;
+    cin >> n;
+    vector<int> ans(n);
+    if (n%2) {
+        for (int i=0; i<n; i++) {
+            ans[i] = i + (n+1)/2 + 2;
+        }
+        ans[0]--;
+        ans[n-1]++;
+        ans[n-2]++;
+    }
+    else {
+        for (int i=0; i<n; i++) {
+            if (i >= n/2) ans[i] = i + n/2 + 1;
+            else ans[i] = i + n/2;
+        }
+    }
+    for (int i=0; i<n; i++) {
+        cout << ans[i] << ' ';
+    }
+    cout << '\n';
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int T = 1;
+    cin >> T;
+    for (int t=1; t<=T; t++) {
+        solve(t);
+    }
+}
