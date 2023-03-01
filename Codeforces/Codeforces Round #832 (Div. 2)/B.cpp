3309e2173fc32cb18ebba213ac9588a4a12d2f33
SeongUk <pkseonguk@gmail.com>
2023-03-01 17:07:51 +0900

    Add A B C D Codeforces Round #832 (Div. 2)

---
+++ Codeforces/Codeforces Round #832 (Div. 2)/B.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+#define all(c) (c).begin(),(c).end()
+#define srt(c) sort(all(c))
+#define srtrev(c) sort(all(c)); reverse(all(c))
+using namespace std;
+using ll=long long;
+using i128 = __int128_t;
+using pi=pair<int, int>;
+using pll=pair<ll, ll>;
+using ti=tuple<int, int, int>;
+using tll=tuple<ll, ll, ll>;
+template <class T> using pq = priority_queue<T>;
+template <class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+ll gcd(ll a, ll b) { for (; b; a %= b, swap(a, b)); return a; }
+void no() { cout << "No" << '\n'; }
+void yes() { cout << "Yes" << '\n'; }
+
+void solve(int CASE) {
+    int n;
+    cin >> n;
+    vector<pi> ans;
+    int l = 1;
+    int r = 3*n;
+    while (l < r) {
+        ans.push_back({l, r});
+        l += 3;
+        r -= 3;
+    }
+    cout << ans.size() << '\n';
+    for (auto [x, y] : ans) {
+        cout << x << ' ' << y << ' ';
+    }
+    cout << '\n';
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int T = 1;
+    cin >> T;
+    for (int t=1; t<=T; t++) {
+        solve(t);
+    }
+}
