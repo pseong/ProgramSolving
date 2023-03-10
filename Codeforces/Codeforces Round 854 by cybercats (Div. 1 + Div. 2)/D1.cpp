ab93446dbe0a4c41440773ae58699c1c9f0ce0b9
SeongUk <pkseonguk@gmail.com>
2023-03-11 01:21:17 +0900

    Fix D1 Codeforces Round 854 by cybercats (Div. 1 + Div. 2)

--- Codeforces/Codeforces Round 854 by cybercats (Div. 1 + Div. 2)/D1.cpp
+++ Codeforces/Codeforces Round 854 by cybercats (Div. 1 + Div. 2)/D1.cpp
@@ -22,19 +22,33 @@ void solve(int CASE) {
     for (int i=1; i<=n; i++) cin >> v[i];
     for (int i=1; i<=k; i++) cin >> cold[i];
     for (int i=1; i<=k; i++) cin >> hot[i];
-    vector<vector<ll>> dp(n + 1, vector<ll>(2, 1e18));
-    dp[1][0] = cold[v[1]];
-    dp[1][1] = cold[v[1]];
-    for (int i=2; i<=n; i++) {
-        dp[i][0] = min(dp[i-1][0], dp[i-1][1]) + ((v[i] == v[i-1]) ? hot[v[i]] : cold[v[i]]);
-        ll sum = 0;
-        for (int j=i-1; j>=1; j--) {
-            dp[i][1] = min(dp[i][1], ((v[j-1] == v[i]) ? hot[v[i]] : cold[v[i]]) + dp[j][1] + sum);
-            if (v[j-1] == v[j]) sum += hot[v[j]];
-            else sum += cold[v[j]];
+    vector<ll> dp(k + 1, 1e18);
+    dp[0] = 0;
+    for (int i=1; i<=n; i++) {
+        vector<ll> ndp(k + 1, 1e18);
+        if (v[i] == v[i-1]) {
+            for (int j=0; j<=k; j++) {
+                ndp[j] = min(ndp[j], dp[j] + hot[v[i]]);
+            }
+            for (int j=0; j<=k; j++) {
+                if (j == v[i]) continue;
+                ndp[v[i]] = min(ndp[v[i]], dp[j] + cold[v[i]]);
+            }
+            ndp[v[i]] = min(ndp[v[i]], dp[v[i]] + hot[v[i]]);
         }
+        else {
+            for (int j=0; j<=k; j++) {
+                ndp[j] = min(ndp[j], dp[j] + cold[v[i]]);
+            }
+            for (int j=0; j<=k; j++) {
+                if (j == v[i]) continue;
+                ndp[v[i-1]] = min(ndp[v[i-1]], dp[j] + cold[v[i]]);
+            }
+            ndp[v[i-1]] = min(ndp[v[i-1]], dp[v[i]] + hot[v[i]]);
+        }
+        dp = ndp;
     }
-    cout << min(dp[n][0], dp[n][1]) << '\n';
+    cout << *min_element(dp.begin(), dp.end()) << '\n';
 }
 
 signed main() {
