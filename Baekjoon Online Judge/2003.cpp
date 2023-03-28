5abac8f44a02590a7d6b9a402a7a20c3071ef7b6
SeongUk <pkseonguk@gmail.com>
2023-03-28 23:03:36 +0900

    Fix 2003

--- Baekjoon Online Judge/2003.cpp
+++ Baekjoon Online Judge/2003.cpp
@@ -1,19 +1,5 @@
 #include <bits/stdc++.h>
-#define all(c) (c).begin(),(c).end()
-#define srt(c) sort(all(c))
-#define srtrev(c) sort(all(c)); reverse(all(c))
 using namespace std;
-using ll=long long;
-using i128 = __int128_t;
-using pi=pair<int, int>;
-using pll=pair<ll, ll>;
-using ti=tuple<int, int, int>;
-using tll=tuple<ll, ll, ll>;
-template <class T> using pq = priority_queue<T>;
-template <class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
-ll gcd(ll a, ll b) { for (; b; a %= b, swap(a, b)); return a; }
-void no() { cout << "No" << '\n'; }
-void yes() { cout << "Yes" << '\n'; }
 
 signed main() {
     ios::sync_with_stdio(false);
@@ -21,24 +7,17 @@ signed main() {
 
     int n, m;
     cin >> n >> m;
-    vector<int> v(n+1);
-    for (int i=1; i<=n; i++) {
+    vector<int> v(n);
+    for (int i=0; i<n; i++) {
         cin >> v[i];
-        v[i] += v[i-1];
     }
-    int l = 1;
-    int r = 1;
     int ans = 0;
-    while (r <= n) {
-        int x = v[r] - v[l-1];
-        if (x >= m) {
-            if (x == m) ans++;
-            l++;
-            if (l > r) r++;
-        }
-        else {
-            r++;
-        }
+    int r = -1;
+    int sum = 0;
+    for (int l=0; l<n; l++) {
+        while (r < n && sum < m) r++, sum += v[r];
+        if (sum == m) ans++;
+        sum -= v[l];
     }
     cout << ans << '\n';
 }
