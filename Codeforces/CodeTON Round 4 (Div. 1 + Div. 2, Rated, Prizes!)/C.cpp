96986ff49566a8e11c1afaa1945d34c0cd044b93
SeongUk <pkseonguk@gmail.com>
2023-04-02 15:36:23 +0900

    Fix C CodeTON Round 4 (Div. 1 + Div. 2, Rated, Prizes!)

--- Codeforces/CodeTON Round 4 (Div. 1 + Div. 2, Rated, Prizes!)/C.cpp
+++ Codeforces/CodeTON Round 4 (Div. 1 + Div. 2, Rated, Prizes!)/C.cpp
@@ -18,43 +18,18 @@ void yes() { cout << "Yes" << '\n'; }
 void solve(int CASE) {
     ll n, c, d;
     cin >> n >> c >> d;
-    vector<ll> v(n + 1);
-    map<ll, ll> mp;
-    for (int i=1; i<=n; i++) {
+    vector<ll> v(n);
+    for (int i=0; i<n; i++) {
         cin >> v[i];
-        mp[v[i]]++;
     }
-    ll ans = 0;
-    if (mp.count(1) == 0) {
-        ans += d;
-        mp[1] = 1;
+    sort(v.begin(), v.end());
+    ll ans = c * n + d;
+    ll t = 0;
+    for (int i=0; i<n; i++) {
+        if (i == 0 || v[i] != v[i-1]) t++;
+        ans = min(ans, c * n + d * v[i] - (c + d) * t);
     }
-    for (auto [x, y] : mp) {
-        if (y > 1) {
-            ans += c * (y - 1);
-        }
-    }
-    set<ll> st;
-    for (auto [x, y] : mp) st.insert(x);
-
-    ll cnt = 0;
-    ll now = 1;
-    ll no = 2;
-    ll res = 1e18;
-    bool ok = true;
-    for (auto x : st) {
-        cnt++;
-        if (x == now) {
-            now++;
-            continue;
-        }
-        if (x != now) {
-            res = min(res, ans + ((ll)st.size() - (cnt - 1)) * c);
-            ans += (x - now) * d;
-            now = x + 1;
-        }
-    }
-    cout << min(ans, res) << '\n';
+    cout << ans << '\n';
 }
 
 signed main() {
