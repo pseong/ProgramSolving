4f21069560f455f8d311e9c36f954bd82ce1df25
SeongUk <pkseonguk@gmail.com>
2023-05-16 23:42:17 +0900

    Add A B C Educational Codeforces Round 148 (Rated for Div. 2)

---
+++ Codeforces/Educational Codeforces Round 148 (Rated for Div. 2)/A.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#define all(c) (c).begin(),(c).end()
+#define srt(c) sort(all(c))
+#define srtrev(c) sort(all(c)); reverse(all(c))
+using namespace std;
+using ll=long long;
+using i128 = __int128_t;
+using pi=pair<int, int>;
+using pll=pair<ll, ll>;
+using ti=tuple<int, int, int>;
+using tll=tuple<ll, ll, ll>;
+template <class T> using pq = priority_queue<T>;
+template <class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+ll gcd(ll a, ll b) { for (; b; a %= b, swap(a, b)); return a; }
+void no() { cout << "No" << '\n'; }
+void yes() { cout << "Yes" << '\n'; }
+
+void solve(int CASE) {
+    string s;
+    cin >> s;
+    map<char, int> mp;
+    for (int i=0; i<(int)s.size(); i++) {
+        mp[s[i]]++;
+    }
+    int cnt = 0;
+    for (auto [x, y] : mp) {
+        if (y > 1) cnt++;
+    }
+    if (cnt > 1) yes();
+    else no();
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int T = 1;
+    cin >> T;
+    for (int t=1; t<=T; t++) {
+        solve(t);
+    }
+}
