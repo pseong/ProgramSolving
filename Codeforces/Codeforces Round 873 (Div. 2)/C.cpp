1b5207be4329cd00f36b0369f607c9e14b8d5331
SeongUk <pkseonguk@gmail.com>
2023-06-08 17:39:59 +0900

    Add A B C D1 Codeforces Round 873 (Div. 2)

---
+++ Codeforces/Codeforces Round 873 (Div. 2)/C.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#define all(c) (c).begin(),(c).end()
+#define srt(c) sort(all(c))
+#define srtrev(c) sort(all(c)); reverse(all(c))
+using namespace std;
+using ll=long long;
+using i128 = __int128_t;
+using pi=pair<int, int>;
+using pll=pair<ll, ll>;
+using ti=tuple<int, int, int>;
+using tll=tuple<ll, ll, ll>;
+template <class T> using pq = priority_queue<T>;
+template <class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+ll gcd(ll a, ll b) { for (; b; a %= b, swap(a, b)); return a; }
+void no() { cout << "No" << '\n'; }
+void yes() { cout << "Yes" << '\n'; }
+
+const ll mod = 1e9 + 7;
+
+void solve(int CASE) {
+    int n;
+    cin >> n;
+    vector<int> a(n), b(n);
+    for (int i=0; i<n; i++) cin >> a[i];
+    for (int i=0; i<n; i++) cin >> b[i];
+    sort(all(a));
+    sort(all(b));
+    ll ans = 1;
+    for (int i=0; i<n; i++) {
+        auto idx = lower_bound(all(b), a[i]) - b.begin();
+        if (idx - i < 0) ans *= 0;
+        ans *= idx - i;
+        ans %= mod;
+    }
+    cout << ans << '\n';    
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int T = 1;
+    cin >> T;
+    for (int t=1; t<=T; t++) {
+        solve(t);
+    }
+}
