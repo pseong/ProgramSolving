0ba2066274473b78447d030918b3151a60d51ca6
SeongUk <pkseonguk@gmail.com>
2023-07-05 09:25:17 +0900

    Fix B Codeforces Round 823 (Div. 2)

--- Codeforces/Codeforces Round 823 (Div. 2)/B.cpp
+++ Codeforces/Codeforces Round 823 (Div. 2)/B.cpp
@@ -21,23 +21,13 @@ void solve(int CASE) {
     vector<int> v(n), t(n);
     for (int i=0; i<n; i++) cin >> v[i];
     for (int i=0; i<n; i++) cin >> t[i];
-    int mx = *max_element(t.begin(), t.end());
-    vector<int> a;
-    for (int i=0; i<n; i++) if (t[i] == mx) a.push_back(v[i]);
-    srt(a);
-    int s = a.front();
-    int e = a.back();
+    int mn = 1e9;
+    int mx = -1e9;
     for (int i=0; i<n; i++) {
-        int g = mx - t[i];
-        if (v[i] <= s) {
-            if (v[i] + g < s) a.push_back(v[i] + g);
-        }
-        else if (v[i] >= e) {
-            if (v[i] - g > e) a.push_back(v[i] - g);
-        }
+        mn = min(mn, v[i] - t[i]);
+        mx = max(mx, v[i] + t[i]);
     }
-    srt(a);
-    cout << fixed << setprecision(16) << (double)(a.back() + a.front()) / 2 << '\n';
+    cout << fixed << setprecision(10) << 0.5 * (mn + mx) << '\n';
 }
 
 signed main() {
