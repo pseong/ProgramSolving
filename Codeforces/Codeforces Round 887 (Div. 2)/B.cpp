05f631671e33a2daf0ec6c28c235c2b66540e9e7
SeongUk <pkseonguk@gmail.com>
2023-07-26 03:06:57 +0900

    Add A B C Codeforces Round 887 (Div. 2)

---
+++ Codeforces/Codeforces Round 887 (Div. 2)/B.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+#define all(c) (c).begin(),(c).end()
+#define srt(c) sort(all(c))
+#define srtrev(c) sort(all(c)); reverse(all(c))
+using namespace std;
+using ll=long long;
+using i128 = __int128_t;
+using pi=pair<int, int>;
+using pll=pair<ll, ll>;
+using ti=tuple<int, int, int>;
+using tll=tuple<ll, ll, ll>;
+template <class T> using pq = priority_queue<T>;
+template <class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+ll gcd(ll a, ll b) { for (; b; a %= b, swap(a, b)); return a; }
+void no() { cout << "No" << '\n'; }
+void yes() { cout << "Yes" << '\n'; }
+
+void solve(int CASE) {
+    ll n, k;
+    cin >> n >> k;
+    if (k >= 50) {
+        cout << 0 << '\n';
+        return;
+    }
+    int ans = 0;
+    vector<ll> v(k + 1);
+    v[k] = n;
+    for (int i=0; i<=n; i++) {
+        v[k-1] = i;
+        for (int j=k-2; j>=1; j--) {
+            v[j] = v[j + 2] - v[j + 1];
+        }
+        bool ok = true;
+        for (int j=2; j<=k; j++) {
+            if (v[j] < 0 || v[j-1] > v[j]) ok = false;
+        }
+        if (ok) ans++;
+    }
+    cout << ans << '\n';
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int T = 1;
+    cin >> T;
+    for (int t=1; t<=T; t++) {
+        solve(t);
+    }
+}
